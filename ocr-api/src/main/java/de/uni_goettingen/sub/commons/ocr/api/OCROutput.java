d2b8abc07863bcf1c48785aef9d34e404dc28ad6
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2010-09-18 22:51:12 +0000

    - added an interface for output definition

--- ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/api/OCROutput.java
+++ ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/api/OCROutput.java
@@ -1,33 +1,54 @@
 package de.uni_goettingen.sub.commons.ocr.api;
 
 import java.net.URL;
+import java.util.Map;
 
 
 /**
- * The Class OCROutput.
- * Output Folder
+ * The Interface OCROutput.
  */
-public class OCROutput {
+public interface OCROutput {
 
-	/** The url. */
-	protected URL url;
-
+	
 	/**
 	 * Gets the url.
 	 *
-	 * @return the url
 	 */
-	protected URL getUrl() {
-		return url;
-	}
+	public URL getUrl();
 
 	/**
 	 * Sets the url.
 	 *
-	 * @param url the new url
 	 */
-	protected void setUrl(URL url) {
-		this.url = url;
-	}
+	public void setUrl(URL url);
+	
+	/**
+	 * Sets the params.
+	 *
+	 * @param params the params
+	 */
+	public void setParams (Map<String, String> params);
+	
+	/**
+	 * Gets the params.
+	 *
+	 * @return the params
+	 */
+	public Map<String, String> getParams();
+	
+	/**
+	 * Sets the format.
+	 *
+	 * @param format the new format
+	 */
+	public void setFormat (OCRFormat format);
+	
+	
+	/**
+	 * Gets the format.
+	 *
+	 * @return the format
+	 */
+	public OCRFormat getFormat ();
 
 }
