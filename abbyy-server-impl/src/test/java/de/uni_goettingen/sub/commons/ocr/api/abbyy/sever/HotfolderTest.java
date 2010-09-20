bbc6180b1a617a19557410d4668e55eaab94d0fc
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2010-09-20 11:03:49 +0000

    mkCol and delete inserted for test

--- abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/api/abbyy/sever/HotfolderTest.java
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/api/abbyy/sever/HotfolderTest.java
@@ -1,17 +1,49 @@
 package de.uni_goettingen.sub.commons.ocr.api.abbyy.sever;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.jackrabbit.webdav.client.methods.MkColMethod;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import de.uni_goettingen.sub.commons.ocr.abbyy.server.Hotfolder;
 
 public class HotfolderTest {
 	
 	@BeforeClass
-	public static void init () {
+	public static void init () throws FileSystemException {
 		
 	}
 	
 	@Test
-	public void testHotfolder () {
+	public void testHotfolder () throws HttpException, IOException, InterruptedException {
+		Hotfolder hot = new Hotfolder();
+		HttpClient client = new HttpClient();
+		Credentials defaultcreds = new UsernamePasswordCredentials("gdz", "***REMOVED***");
+	    client.getState().setCredentials(AuthScope.ANY, defaultcreds);
+	    hot.setClient(client);
+	    
+	    //hot.copyFilesToServer(files);
+	   
+	    // Ok
+	    hot.mkCol("http://dl380-130.gbv.de/webdav/GDZ/input/test/");
+	    
+	    // 
+	    //hot.put(new URL("http://dl380-130.gbv.de/webdav/GDZ/input/test.xml/"), new File("C:/test.xml/") );
+	   
+	    //ok
+	    hot.delete("http://dl380-130.gbv.de/webdav/GDZ/input/test/");
 		
+	    
+	    
+	    
 	}
 }
