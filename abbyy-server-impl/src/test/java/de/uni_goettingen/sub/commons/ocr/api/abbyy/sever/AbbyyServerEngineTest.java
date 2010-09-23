1174d79cb72fcf7585c0d3fefb569276d6d4ebfb
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2010-09-23 10:52:07 +0000

    AbbyyServerEnginsTest inserted test ok

---
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/api/abbyy/sever/AbbyyServerEngineTest.java
@@ -0,0 +1,40 @@
+package de.uni_goettingen.sub.commons.ocr.api.abbyy.sever;
+
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.vfs.FileSystemException;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_goettingen.sub.commons.ocr.abbyy.server.AbbyyServerEngine;
+import de.uni_goettingen.sub.commons.ocr.abbyy.server.Hotfolder;
+
+
+
+
+public class AbbyyServerEngineTest {
+	public static AbbyyServerEngine abbyy;
+	public Hotfolder hotfolder;
+	
+	@Before
+	public  void init () throws ConfigurationException, FileSystemException {
+		PropertiesConfiguration config = new PropertiesConfiguration("config-properties");
+		//abbyy = mock(AbbyyServerEngine.class);
+		abbyy = new AbbyyServerEngine();
+		abbyy.loadConfig(config);
+	}
+	
+	@Test
+	public void testCli () throws IOException {	
+		
+		abbyy.checkServerState();
+	}
+
+	
+	
+}
+
