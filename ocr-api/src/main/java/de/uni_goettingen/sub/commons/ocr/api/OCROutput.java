c92ec16d66a5265e9132e085d26ef62390fe34cf
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2010-10-01 13:45:32 +0000

    - Cleanup

--- ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/api/OCROutput.java
+++ ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/api/OCROutput.java
@@ -3,50 +3,49 @@ package de.uni_goettingen.sub.commons.ocr.api;
 import java.net.URL;
 import java.util.Map;
 
-
 /**
  * The Interface OCROutput.
  */
 public interface OCROutput {
 
-	
 	/**
 	 * Gets the url.
-	 *
+	 * 
 	 */
-	public URL getUrl();
+	public URL getUrl ();
 
 	/**
 	 * Sets the url.
-	 *
+	 * 
 	 */
-	public void setUrl(URL url);
-	
+	public void setUrl (URL url);
+
 	/**
 	 * Sets the params.
-	 *
-	 * @param params the params
+	 * 
+	 * @param params
+	 *            the params
 	 */
 	public void setParams (Map<String, String> params);
-	
+
 	/**
 	 * Gets the params.
-	 *
+	 * 
 	 * @return the params
 	 */
-	public Map<String, String> getParams();
-	
+	public Map<String, String> getParams ();
+
 	/**
 	 * Sets the format.
-	 *
-	 * @param format the new format
+	 * 
+	 * @param format
+	 *            the new format
 	 */
 	public void setFormat (OCRFormat format);
-	
-	
+
 	/**
 	 * Gets the format.
-	 *
+	 * 
 	 * @return the format
 	 */
 	public OCRFormat getFormat ();
