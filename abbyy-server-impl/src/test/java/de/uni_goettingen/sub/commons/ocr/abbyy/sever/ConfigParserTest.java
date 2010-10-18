e576924787725473afe66f2382005f046fbbf2f2
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2010-10-18 14:08:19 +0000

    - fixed test

--- abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/sever/ConfigParserTest.java
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/sever/ConfigParserTest.java
@@ -2,10 +2,8 @@ package de.uni_goettingen.sub.commons.ocr.abbyy.sever;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
-import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.commons.configuration.ConfigurationException;
@@ -24,27 +22,24 @@ public class ConfigParserTest {
 
 	@BeforeClass
 	public static void init () throws ConfigurationException {
-		
 		try {
 			cp = new ConfigParser().loadConfig();
 		} catch (RuntimeException e) {
 			logger.info("If run from Maven an Exception is expected", e);
 		}
-		assertNull(cp);
+		assertNotNull(cp);
 	}
 	
 	@Test
 	public void testUrl () throws ConfigurationException {
-		URL url = getClass().getResource("/" + ConfigParser.DEFAULT_CONFIG);
-		cp = new ConfigParser(url).loadConfig();
+		cp = new ConfigParser().loadConfig();
 		assertFalse(cp.getDebugAuth());
 	}
 
 	@Test
 	public void testAuth () throws ConfigurationException {
 		System.setProperty(ConfigParser.DEBUG_PROPERTY, "true");
-		URL url = getClass().getResource("/" + ConfigParser.DEFAULT_CONFIG);
-		cp = new ConfigParser(url).loadConfig();
+		cp = new ConfigParser().loadConfig();
 		assertTrue(cp.getDebugAuth());
 
 	}
