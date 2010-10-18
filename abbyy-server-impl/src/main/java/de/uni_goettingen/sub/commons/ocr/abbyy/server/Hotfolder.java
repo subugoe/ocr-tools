e56c445a0eee3ad304d1aaf7f7925f083b894443
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2010-10-18 01:04:33 +0000

    - cleanup

--- abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/Hotfolder.java
+++ abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/Hotfolder.java
@@ -18,144 +18,130 @@ package de.uni_goettingen.sub.commons.ocr.abbyy.server;
 
  */
 
-
-
-
-
-
 import java.io.File;
 import java.io.IOException;
-
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-
 import org.apache.commons.vfs.AllFileSelector;
-import org.apache.commons.vfs.FileObject;
-
 import org.apache.commons.vfs.FileContent;
+import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.FileSystemManager;
 import org.apache.commons.vfs.VFS;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
-
 
 /**
  * The Class Hotfolder is used to control the hotfolders used by the Abbyy
  * Recognition Server.
  */
-public class Hotfolder extends Thread{
+public class Hotfolder extends Thread {
 
 	/** The errror, input, output folder. */
 	protected URL inFolder, outFolder, errrorFolder;
-	
+
 	/** The url file. */
 	FileObject urlFile = null;
-	
+
 	/** The url file string to url. */
 	FileObject urlFileStringToUrl = null;
-	
+
 	/** The get url image. */
 	FileObject getUrlImage = null;
-	
+
 	/** The total size. */
 	Long totalSize = 0l;
-	
+
 	/** The image directory. */
 	protected String imageDirectory;
-	
+
 	/** The identifier. */
 	protected String identifier;
 	//protected HierarchicalConfiguration configu;
 	/** The language. */
 	protected static List<Locale> langs;
-	
+
 	/** The local output dir. */
 	protected static String localOutputDir = null;
-	
+
 	/** The fileinfos. */
 	protected List<AbbyyOCRImage> fileInfos = null;
-	
+
 	protected String webdavURL;
-	protected String inputFolder ;
-	protected String outputFolder ;
+	protected String inputFolder;
+	protected String outputFolder;
 	protected String errorFolder;
-	
+
 	//protected static Boolean writeRemotePrefix = true;
 
 	/** The Constant logger. */
 	final static Logger logger = LoggerFactory.getLogger(Hotfolder.class);
-	
+
 	/** The fsmanager. */
 	FileSystemManager fsManager = null;
 
 	/**
 	 * Instantiates a new hotfolder.
-	 *
-	 * @throws FileSystemException the file system exception
+	 * 
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
 	public Hotfolder() throws FileSystemException {
 		fsManager = VFS.getManager();
 	}
-	
-	
-	
+
 	/**
 	 * Copy a url from source to destination. Assumes overwrite.
-	 *
-	 * @param files is a List of The Class AbbyyOCRImage. Is a representation of an 
-	 * OCRImage suitable for holding references to remote files as used by the Abbyy 
-	 * Recognition Server
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 * @throws InterruptedException the interrupted exception
-	 * @throws FileSystemException 
+	 * 
+	 * @param files
+	 *            is a List of The Class AbbyyOCRImage. Is a representation of
+	 *            an OCRImage suitable for holding references to remote files as
+	 *            used by the Abbyy Recognition Server
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * @throws InterruptedException
+	 *             the interrupted exception
+	 * @throws FileSystemException
 	 */
-	public void copyFilesToServer(List<AbbyyOCRImage> files) throws
-			InterruptedException, FileSystemException {
+	public void copyFilesToServer (List<AbbyyOCRImage> files) throws InterruptedException, FileSystemException {
 		// iterate over all Files and put them to Abbyy-server inputFolder:
 		for (AbbyyOCRImage info : files) {
 			/* File f = urlToFile(AbbyyFileName); */
-			FileObject remoteFile = fsManager.resolveFile(info.getRemoteURL()
-					.toString());
-			FileObject imageUrlfile = fsManager.resolveFile(info.getUrl()
-					.toString());
+			FileObject remoteFile = fsManager.resolveFile(info.getRemoteURL().toString());
+			FileObject imageUrlfile = fsManager.resolveFile(info.getUrl().toString());
 			// Delete if exists
 			deleteIfExists(info.getRemoteURL());
 			if (info.toString().endsWith("/")) {
-				logger.trace("Creating new directory "
-						+ info.getRemoteURL().toString() + "!");
+				logger.trace("Creating new directory " + info.getRemoteURL().toString() + "!");
 				// Create the directory
 				mkCol(info.getRemoteURL());
 
 			} else {
 
-				logger.trace("Copy from " + info.getUrl().toString() + " to "
-						+ info.getRemoteURL());
+				logger.trace("Copy from " + info.getUrl().toString() + " to " + info.getRemoteURL());
 				remoteFile.copyFrom(imageUrlfile, new AllFileSelector());
 			}
 		}
 	}
-	
+
 	/**
 	 * Copy a files from remotefile to localfile. Assumes overwrite.
 	 * 
-	 * @param remotefile, the url of the file name as used on the remote system, usally a
-	 * relative file name and thus represented as a String
-	 * @param localfile, an URL representing the local file, it should be resolveable
-	 * from the local Server.
-	 * @throws FileSystemException the file system exception
+	 * @param remotefile
+	 *            , the url of the file name as used on the remote system,
+	 *            usally a relative file name and thus represented as a String
+	 * @param localfile
+	 *            , an URL representing the local file, it should be resolveable
+	 *            from the local Server.
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public void copyAllFiles(String remotefile, String localfile) throws FileSystemException{
+	public void copyAllFiles (String remotefile, String localfile) throws FileSystemException {
 		FileObject remoteFile = fsManager.resolveFile(remotefile);
 		FileObject localFile = fsManager.resolveFile(localfile);
 		localFile.copyFrom(remoteFile, new AllFileSelector());
@@ -163,56 +149,68 @@ public class Hotfolder extends Thread{
 
 	/**
 	 * Delete a resource at the specified url
-	 *
-	 * @param url the url
-	 * @throws FileSystemException the file system exception
+	 * 
+	 * @param url
+	 *            the url
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public void delete(URL url) throws FileSystemException {
+	public void delete (URL url) throws FileSystemException {
 		fsManager.resolveFile(url.toString()).delete();
 	}
 
 	/**
 	 * Delete a resource at the specified url if exists.
-	 *
-	 * @param url the url
-	 * @throws FileSystemException the file system exception
+	 * 
+	 * @param url
+	 *            the url
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public void deleteIfExists(URL url) throws FileSystemException {
-		if (fsManager.resolveFile(url.toString()).delete())
+	public void deleteIfExists (URL url) throws FileSystemException {
+		if (fsManager.resolveFile(url.toString()).delete()) {
 			logger.debug(url.toString() + " Exists already but now deleted");
+		}
 	}
 
 	/**
 	 * Delete a resource at the specified url(String) if exists.
-	 *
-	 * @param url the url
-	 * @throws FileSystemException the file system exception
+	 * 
+	 * @param url
+	 *            the url
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public void deleteIfExists(String url) throws FileSystemException {
-		if (fsManager.resolveFile(url).delete())
+	public void deleteIfExists (String url) throws FileSystemException {
+		if (fsManager.resolveFile(url).delete()) {
 			logger.debug(url + " Exists already but now deleted");
+		}
 
 	}
-	
+
 	/**
 	 * to create a directory at the specified url
-	 *
-	 * @param url the url
-	 * @throws FileSystemException the file system exception
+	 * 
+	 * @param url
+	 *            the url
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public void mkCol(URL url) throws FileSystemException {
+	public void mkCol (URL url) throws FileSystemException {
 		fsManager.resolveFile(url.toString()).createFolder();
 		logger.debug(url.toString() + " created");
 	}
 
 	/**
 	 * a resource at the specified url if exists.
-	 *
-	 * @param url the url
+	 * 
+	 * @param url
+	 *            the url
 	 * @return true, if successful
-	 * @throws FileSystemException the file system exception
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public boolean fileIfexists(String url) throws FileSystemException {
+	public boolean fileIfexists (String url) throws FileSystemException {
 		if (fsManager.resolveFile(url).exists()) {
 			return true;
 		} else {
@@ -222,12 +220,14 @@ public class Hotfolder extends Thread{
 
 	/**
 	 * Gets the total size for a url.
-	 *
-	 * @param url the url
+	 * 
+	 * @param url
+	 *            the url
 	 * @return the total size
-	 * @throws FileSystemException the file system exception
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public Long getTotalSize(URL url) throws FileSystemException {
+	public Long getTotalSize (URL url) throws FileSystemException {
 		totalSize++;
 		urlFile = fsManager.resolveFile(url.toString());
 
@@ -251,172 +251,161 @@ public class Hotfolder extends Thread{
 
 	/**
 	 * String to url.
-	 *
-	 * @param uri the uri
+	 * 
+	 * @param uri
+	 *            the uri
 	 * @return the uRL
-	 * @throws FileSystemException the file system exception
+	 * @throws FileSystemException
+	 *             the file system exception
 	 */
-	public URL stringToUrl (String uri) throws FileSystemException{
+	public URL stringToUrl (String uri) throws FileSystemException {
 		urlFileStringToUrl = fsManager.resolveFile(uri);
 		return urlFileStringToUrl.getURL();
-		
+
 	}
-	
+
 	/**
 	 * Gets the url list.
-	 *
-	 * @param imageDirectory the image directory
+	 * 
+	 * @param imageDirectory
+	 *            the image directory
 	 * @return the url list
-	 * @throws FileSystemException the file system exception
-	 * @throws MalformedURLException the malformed url exception
+	 * @throws FileSystemException
+	 *             the file system exception
+	 * @throws MalformedURLException
+	 *             the malformed url exception
 	 */
-	List<AbbyyOCRImage> getUrlList(String imageDirectory) throws FileSystemException, MalformedURLException {
+	List<AbbyyOCRImage> getUrlList (String imageDirectory) throws FileSystemException, MalformedURLException {
 		List<AbbyyOCRImage> imageList = new ArrayList<AbbyyOCRImage>();
 		getUrlImage = fsManager.resolveFile(imageDirectory);
 		FileObject[] children = getUrlImage.getChildren();
-		 for ( int i = 0; i < children.length; i++ )
-	        {
-			 imageList.add(new AbbyyOCRImage(new URL(children[ i ].getName().toString())));
-	        }
+		for (int i = 0; i < children.length; i++) {
+			imageList.add(new AbbyyOCRImage(new URL(children[i].getName().toString())));
+		}
 		return imageList;
 	}
-	
+
 	/**
 	 * File to url.
-	 *
-	 * @param file the file
+	 * 
+	 * @param file
+	 *            the file
 	 * @return the uRL
 	 */
-	public URL fileToURL(File file){
-        URL url = null;
-        try {
-            url = new URL("file://" + file.getPath());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return url;
-    } 
-	
+	public URL fileToURL (File file) {
+		URL url = null;
+		try {
+			url = new URL("file://" + file.getPath());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return url;
+	}
+
 	/**
 	 * Url to file.
-	 *
-	 * @param url the url
+	 * 
+	 * @param url
+	 *            the url
 	 * @return the file
 	 */
-	public File urlToFile(URL url) {
-        File file = null;
-        try {
-          file = new File(url.toURI());
-        } catch(URISyntaxException e) {
-          file = new File(url.getPath());
-        }
-        return file;
-    } 
-	
-	
-	
-	
+	public File urlToFile (URL url) {
+		File file = null;
+		try {
+			file = new File(url.toURI());
+		} catch (URISyntaxException e) {
+			file = new File(url.getPath());
+		}
+		return file;
+	}
+
 	/**
 	 * Gets the in folder.
-	 *
+	 * 
 	 * @return the in folder
 	 */
-	public URL getInFolder() {
+	public URL getInFolder () {
 		return inFolder;
 	}
 
 	/**
 	 * Sets the in folder.
-	 *
-	 * @param inFolder the new in folder
+	 * 
+	 * @param inFolder
+	 *            the new in folder
 	 */
-	public void setInFolder(URL inFolder) {
+	public void setInFolder (URL inFolder) {
 		this.inFolder = inFolder;
 	}
 
 	/**
 	 * Gets the out folder.
-	 *
+	 * 
 	 * @return the out folder
 	 */
-	public URL getOutFolder() {
+	public URL getOutFolder () {
 		return outFolder;
 	}
 
 	/**
 	 * Sets the out folder.
-	 *
-	 * @param outFolder the new out folder
+	 * 
+	 * @param outFolder
+	 *            the new out folder
 	 */
-	public void setOutFolder(URL outFolder) {
+	public void setOutFolder (URL outFolder) {
 		this.outFolder = outFolder;
 	}
 
 	/**
 	 * Gets the errror folder.
-	 *
+	 * 
 	 * @return the errror folder
 	 */
-	public URL getErrrorFolder() {
+	public URL getErrrorFolder () {
 		return errrorFolder;
 	}
 
 	/**
 	 * Sets the errror folder.
-	 *
-	 * @param errrorFolder the new errror folder
+	 * 
+	 * @param errrorFolder
+	 *            the new errror folder
 	 */
-	public void setErrrorFolder(URL errrorFolder) {
+	public void setErrrorFolder (URL errrorFolder) {
 		this.errrorFolder = errrorFolder;
 	}
 
-
-
-	public String getWebdavURL() {
+	public String getWebdavURL () {
 		return webdavURL;
 	}
 
-
-
-	public void setWebdavURL(String webdavURL) {
+	public void setWebdavURL (String webdavURL) {
 		this.webdavURL = webdavURL;
 	}
 
-
-
-	public String getInputFolder() {
+	public String getInputFolder () {
 		return inputFolder;
 	}
 
-
-
-	public void setInputFolder(String inputFolder) {
+	public void setInputFolder (String inputFolder) {
 		this.inputFolder = inputFolder;
 	}
 
-
-
-	public String getOutputFolder() {
+	public String getOutputFolder () {
 		return outputFolder;
 	}
 
-
-
-	public void setOutputFolder(String outputFolder) {
+	public void setOutputFolder (String outputFolder) {
 		this.outputFolder = outputFolder;
 	}
 
-
-
-	public String getErrorFolder() {
+	public String getErrorFolder () {
 		return errorFolder;
 	}
 
-
-
-	public void setErrorFolder(String errorFolder) {
+	public void setErrorFolder (String errorFolder) {
 		this.errorFolder = errorFolder;
 	}
 
-	
 }
