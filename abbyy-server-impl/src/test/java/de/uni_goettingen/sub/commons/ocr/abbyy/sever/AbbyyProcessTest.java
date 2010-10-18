f6652b000627898ac2a0cc590cdd0175d05aa2a5
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2010-10-18 08:31:43 +0000

    - cleanups

--- abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/sever/AbbyyProcessTest.java
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/sever/AbbyyProcessTest.java
@@ -32,27 +32,28 @@ public class AbbyyProcessTest {
 	public static File BASEFOLDER_FILE = TicketTest.BASEFOLDER_FILE;
 	public static List<String> TEST_FOLDERS;
 
-	
 	static {
-		TEST_FOLDERS = new ArrayList<String>(){{
-			add("PPN129323640_0010");
-			add("PPN31311157X_0102");
-			add("PPN514401303_1890");
-			add("PPN514854804_0001");
-		}};
+		TEST_FOLDERS = new ArrayList<String>() {
+			{
+				add("PPN129323640_0010");
+				add("PPN31311157X_0102");
+				add("PPN514401303_1890");
+				add("PPN514854804_0001");
+			}
+		};
 	}
-	
+
 	@BeforeClass
 	public static void init () {
 		logger.debug("Starting AbbyyProcessTest");
 		//Nothing to do here
 	}
-	
+
 	@Test
 	public void createAbbyyProcess () throws IOException {
-		for (String book: TEST_FOLDERS) {
+		for (String book : TEST_FOLDERS) {
 			File testDir = new File(BASEFOLDER_FILE.getAbsoluteFile() + File.separator + HotfolderTest.INPUT + File.separator + book);
-			logger.debug("Creating AbbyyProcess for "+ testDir.getAbsolutePath());
+			logger.debug("Creating AbbyyProcess for " + testDir.getAbsolutePath());
 			AbbyyProcess aop = AbbyyProcess.createProcessFromDir(testDir, TicketTest.EXTENSION);
 			assertNotNull(aop);
 			aop.setOcrOutput(TicketTest.OUTPUT_DEFINITIONS);
@@ -60,14 +61,14 @@ public class AbbyyProcessTest {
 			aop.write(testTicket, testDir.getName());
 			logger.debug("Wrote Ticket:\n" + TicketTest.dumpTicket(new FileInputStream(testTicket)));
 		}
-		
+
 	}
-	
+
 	@Test
 	public void checkTicketCount () throws IOException, XmlException {
-		for (String book: TEST_FOLDERS) {
+		for (String book : TEST_FOLDERS) {
 			File testDir = new File(BASEFOLDER_FILE.getAbsoluteFile() + File.separator + HotfolderTest.INPUT + File.separator + book);
-			logger.debug("Creating AbbyyProcess for "+ testDir.getAbsolutePath());
+			logger.debug("Creating AbbyyProcess for " + testDir.getAbsolutePath());
 			AbbyyProcess aop = AbbyyProcess.createProcessFromDir(testDir, TicketTest.EXTENSION);
 			assertNotNull(aop);
 			aop.setOcrOutput(TicketTest.OUTPUT_DEFINITIONS);
@@ -77,7 +78,7 @@ public class AbbyyProcessTest {
 			assertTrue("This fails if the number of files between ticket and file system differs.", TicketTest.parseFilesFromTicket(testTicket).size() == aop.getOcrImages().size());
 		}
 	}
-	
+
 	@Test
 	public void createProcessViaAPI () throws MalformedURLException {
 		AbbyyServerEngine ase = AbbyyServerEngine.getInstance();
@@ -97,18 +98,18 @@ public class AbbyyProcessTest {
 		op.setOcrImages(imgList);
 		AbbyyProcess aop = (AbbyyProcess) op;
 		//aop.write(out, identifier)
-		
+
 	}
-	
+
 	@AfterClass
 	public static void cleanup () {
 		logger.debug("Cleaning up");
-		for (String book: TEST_FOLDERS) {
+		for (String book : TEST_FOLDERS) {
 			File testTicket = new File(BASEFOLDER_FILE.getAbsoluteFile() + File.separator + HotfolderTest.INPUT + File.separator + book + ".xml");
 			logger.equals("Deleting file " + testTicket.getAbsolutePath());
 			testTicket.delete();
 			assertTrue(!testTicket.exists());
 		}
 	}
-	
+
 }
