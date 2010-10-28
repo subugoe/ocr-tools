faf993b108abd3e55b35162cc7deacef4fb715c0
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2010-10-28 15:22:47 +0000

    - moved more hotfolder stuff

---
+++ abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/hotfolder/HotfolderState.java
@@ -0,0 +1,9 @@
+package de.uni_goettingen.sub.commons.ocr.abbyy.server.hotfolder;
+
+public abstract class HotfolderState {
+
+	abstract Long getTotalSize();
+	
+	abstract Long getTotalCount();
+	
+}
