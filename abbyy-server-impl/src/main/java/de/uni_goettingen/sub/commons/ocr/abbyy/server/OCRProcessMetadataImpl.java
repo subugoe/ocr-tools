b807bafb96bf373077c464c6324c6a1662a37b51
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2010-11-19 11:25:50 +0000

    added OCRProcessMetadataImpl

---
+++ abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/OCRProcessMetadataImpl.java
@@ -0,0 +1,117 @@
+package de.uni_goettingen.sub.commons.ocr.abbyy.server;
+
+/*
+
+© 2010, SUB Göttingen. All rights reserved.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Locale;
+
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata;
+
+
+
+
+public class OCRProcessMetadataImpl implements OCRProcessMetadata{
+
+	@Override
+	public String getEncoding() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLinebreak() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDocumentType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDocumentTypeVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSoftwareName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSoftwareVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Locale> getLanguages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getScripts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTextNote() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProcessingNote() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigDecimal getCharacterAccuracy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigDecimal getWordAccuracy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getDuration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
