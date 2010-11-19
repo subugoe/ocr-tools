26c3e026a3b8b8d470bf004ec9e5e9deebcb3465
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2010-11-19 11:29:57 +0000

    added comments

--- abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/OCRProcessMetadataImpl.java
+++ abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/OCRProcessMetadataImpl.java
@@ -23,91 +23,146 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Locale;
 
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess;
 import de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata;
 
 
 
 
+
+/**
+ * The Class OCRProcessMetadataImpl. used to obtain a description of the
+ * {@link OCRProcess} and it's results This can be used to filter the results
+ * for accuracy or to save it for further processing.
+ */
 public class OCRProcessMetadataImpl implements OCRProcessMetadata{
 
+	/**
+	 * Instantiates a new oCR process metadata impl.
+	 */
+	public OCRProcessMetadataImpl() {
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getEncoding()
+	 */
 	@Override
 	public String getEncoding() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getLinebreak()
+	 */
 	@Override
 	public String getLinebreak() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getFormat()
+	 */
 	@Override
 	public String getFormat() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getDocumentType()
+	 */
 	@Override
 	public String getDocumentType() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getDocumentTypeVersion()
+	 */
 	@Override
 	public String getDocumentTypeVersion() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getSoftwareName()
+	 */
 	@Override
 	public String getSoftwareName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getSoftwareVersion()
+	 */
 	@Override
 	public String getSoftwareVersion() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getLanguages()
+	 */
 	@Override
 	public List<Locale> getLanguages() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getScripts()
+	 */
 	@Override
 	public List<String> getScripts() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getTextNote()
+	 */
 	@Override
 	public String getTextNote() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getProcessingNote()
+	 */
 	@Override
 	public String getProcessingNote() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getCharacterAccuracy()
+	 */
 	@Override
 	public BigDecimal getCharacterAccuracy() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getWordAccuracy()
+	 */
 	@Override
 	public BigDecimal getWordAccuracy() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata#getDuration()
+	 */
 	@Override
 	public Long getDuration() {
 		// TODO Auto-generated method stub
