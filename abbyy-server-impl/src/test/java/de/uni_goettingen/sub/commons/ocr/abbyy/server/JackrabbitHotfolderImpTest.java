6df464ed7f2152936f01a16447df3fc35f77b9ee
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2010-11-29 16:44:29 +0000

    added testOpenInputStream method

--- abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/JackrabbitHotfolderImpTest.java
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/JackrabbitHotfolderImpTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.InputStream;
+import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -23,14 +25,23 @@ import org.mortbay.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.abbyy.recognitionServer10Xml.xmlResultSchemaV1.XmlResultDocument;
+import com.abbyy.recognitionServer10Xml.xmlResultSchemaV1.XmlResultDocument.XmlResult;
+
 import de.uni_goettingen.sub.commons.ocr.abbyy.server.hotfolder.JackrabbitHotfolderImpl;
 
-/* * WebDAV Wagon Test
+/* * WebDAV Test
+ * JackrabbitHotfolderImpTest
  * @author abergna
  */
 public class JackrabbitHotfolderImpTest {
 	static Server server;
 	private Context rootContext;
+	private static InputStream isResult = null;
+	protected static XmlResultDocument xmlResultDocument; 
+	XmlResult xm ;
+
+	
 	static JackrabbitHotfolderImpl imp;
 	static File WEBDAV;
 	final static Logger logger = LoggerFactory
@@ -40,7 +51,8 @@ public class JackrabbitHotfolderImpTest {
 	public void setUp() throws Exception {
 		File file = getBaseFolderAsFile();
 	    WEBDAV = new File(file.toString() + "/WEBDAV");
-		server = new Server(8090);
+		WEBDAV.mkdir();
+	    server = new Server(8090);
 		ServletHolder davServletHolder = new ServletHolder(new DAVServlet());
 		davServletHolder.setInitParameter("rootPath", WEBDAV.toString()
 				.replace("file:/", ""));
@@ -66,24 +78,40 @@ public class JackrabbitHotfolderImpTest {
 		assertTrue(imp.exists(new URI("http://localhost:8090/TestA")));
 		assertTrue(imp.exists(new URI("http://localhost:8090/TestB")));
 	}
-	
-	@Test
-	public void testcopyfileFromLocalToServer() throws Exception {
-		WEBDAV = new File(WEBDAV.toString().replace("file:/", "")+ "/"+ "from/test");
-		URI from = WEBDAV.toURI();
-		URI to = new URI("http://localhost:8090/to/test/");
-		imp.copyFile(from, to);
-		assertFalse(imp.exists(new URI("http://localhost:8090/TestB")));
-	}
+		
 	
 	@Test
 	public void testcopyfileFromServerToLocal() throws Exception {
 		WEBDAV = new File(WEBDAV.toString().replace("file:/", "")+ "/"+ "from/FromTO");
 		URI to = WEBDAV.toURI();
-		URI from = new URI("http://localhost:8090/to/FromTO/");
+		URI from = new URI("http://localhost:8090/to/FromTO");
 		imp.copyFile(from, to);
-		assertFalse(imp.exists(to));
+		assertTrue(new File(WEBDAV.toString()).exists());
 	}
+	
+	@Test
+	public void testcopyfileFromLocalToServer() throws Exception {
+		WEBDAV = new File(WEBDAV.toString().replace("file:/", "")+ "/"+ "from/FromTO");
+		URI from = WEBDAV.toURI();
+		URI to = new URI("http://localhost:8090/TestA/FromTO");
+		imp.copyFile(from, to);
+		assertTrue(imp.exists(to));
+	}
+	
+	@Test
+	public void testOpenInputStream() throws Exception {
+		assertTrue(imp.exists(new URI("http://localhost:8090/xmlresult.xml.result.xml")));
+		isResult = imp.openInputStream(new URI("http://localhost:8090/xmlresult.xml.result.xml"));
+		assertTrue(isResult != null);
+		/*xmlResultDocument = XmlResultDocument.Factory.parse(isResult);
+		xm = xmlResultDocument.getXmlResult();
+		BigDecimal b = new BigDecimal(xm.getStatistics().getTotalCharacters());
+	    String str = b.toString();
+	    System.out.println(str);
+	    assertTrue(str.equals("1805558") );*/
+		
+	}
+	
 	
 	@Test
 	public void testdelete() throws Exception {
@@ -98,12 +126,16 @@ public class JackrabbitHotfolderImpTest {
 		
 		imp.deleteIfExists(new URI("http://localhost:8090/to/test"));
 		assertFalse(imp.exists(new URI("http://localhost:8090/to/test")));
+		
+		imp.deleteIfExists(new URI("http://localhost:8090/from/FromTO"));
+		assertFalse(imp.exists(new URI("http://localhost:8090/from/FromTO")));
 	}
+	
 	
 	@After
 	public void destroy() throws Exception {
 		server.stop();
-		System.out.println("Server Stop");
+		System.out.println("Server Stopped");
 	}
 
 	public static File getBaseFolderAsFile() {
