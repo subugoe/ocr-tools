08d8b5803a04ca29d4dcd03d873ca7b2a2937fa2
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2010-12-22 15:23:39 +0000

    added XmlOptions in save methods

--- abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/SerializerTextMD.java
+++ abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/SerializerTextMD.java
@@ -1,18 +1,20 @@
 package de.uni_goettingen.sub.commons.ocr.abbyy.server;
 
 import java.io.File;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
+import java.util.Locale;
+
 import java.util.Map;
 
 import noNamespace.TextMDDocument;
 import noNamespace.TextMDDocument.TextMD;
 import noNamespace.TextMDDocument.TextMD.CharacterInfo;
 import noNamespace.TextMDDocument.TextMD.CharacterInfo.Linebreak.Enum;
+import noNamespace.TextMDDocument.TextMD.Encoding;
+import noNamespace.TextMDDocument.TextMD.Language;
 
 
 
@@ -22,10 +24,12 @@ import org.apache.xmlbeans.XmlOptions;
 
 import de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata;
 
+
+@SuppressWarnings("serial")
 public class SerializerTextMD {
 	
 	protected Map<String, File> tmpfiles = new HashMap<String, File>();
-	public final static String NAMESPACE = "info:lc/xmlns/textMD-v3 http://www.loc.gov/standards/textMD/textMD-v3.01a.xsd";
+	public final static String NAMESPACE = "info:lc/xmlns/textMD-v3";
 	private static XmlOptions opts = new XmlOptions();
 	protected OCRProcessMetadata ocrProcessMetadata;	
 
@@ -36,36 +40,45 @@ public class SerializerTextMD {
 	
 	
 	
-	public void write(){
+	public void write(File file) throws IOException{
 		
+		opts.setSavePrettyPrint();
+		opts.setSaveImplicitNamespaces(new HashMap<String, String>() {
+			{
+				put("", NAMESPACE);
+			}
+		});
+		opts.setUseDefaultNamespace();
 		TextMDDocument textMDDoc = TextMDDocument.Factory.newInstance(opts);
 		TextMD textMD = textMDDoc.addNewTextMD();
-			
+		
+		
+		
+		Encoding encoding =textMD.addNewEncoding();
+		encoding.addNewEncodingPlatform().setLinebreak(noNamespace.TextMDDocument.TextMD.Encoding.EncodingPlatform.Linebreak.Enum.forString(ocrProcessMetadata.getLinebreak()));
+	
 		CharacterInfo characterInfo = textMD.addNewCharacterInfo();
 		if(ocrProcessMetadata.getLinebreak() != null){
 			characterInfo.setLinebreak(Enum.forString(ocrProcessMetadata.getLinebreak()));
 		}
-			
-		if(ocrProcessMetadata.getProcessingNote() != null){
+		
+		
+		
+		/*if(ocrProcessMetadata.getProcessingNote() != null){
 			textMD.addNewProcessingNote();
 			textMD.setProcessingNoteArray(0, ocrProcessMetadata.getProcessingNote());
-		}
+		}*/
 	
 		if(ocrProcessMetadata.getTextNote() != null){
 			textMD.addNewTextNote();
-			textMD.setTextNoteArray(0, ocrProcessMetadata.getProcessingNote());
+			textMD.setTextNoteArray(0, ocrProcessMetadata.getTextNote());
 		}
 		
-		//TODO soll weiter gemacht werden
-		
+	
+		textMD.save(file,opts);
 		
 	}
 	
 	
-	public OutputStream createTmpFile (String name) throws IOException {
-		File tmpFile = File.createTempFile(name, null);
-		tmpfiles.put(name, tmpFile);
-		return new FileOutputStream(tmpFile);
-	}
 	
 }
