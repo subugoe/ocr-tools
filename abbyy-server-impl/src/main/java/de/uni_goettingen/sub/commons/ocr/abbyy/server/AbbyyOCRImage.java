653a5529ac5f13442c1838d2eb6698967a048db1
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2011-01-19 14:35:44 +0000

    remoteFileName removed und all Classes are now formatted

--- abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/AbbyyOCRImage.java
+++ abbyy-server-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/AbbyyOCRImage.java
@@ -2,28 +2,27 @@ package de.uni_goettingen.sub.commons.ocr.abbyy.server;
 
 /*
 
-© 2010, SUB Goettingen. All rights reserved.
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ © 2010, SUB Goettingen. All rights reserved.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-*/
+ */
 
 import java.net.URI;
 
 import de.uni_goettingen.sub.commons.ocr.api.AbstractOCRImage;
 import de.uni_goettingen.sub.commons.ocr.api.OCRImage;
 import de.uni_goettingen.sub.commons.ocr.api.OCRImage.Orientation;
-
 
 /**
  * The Class AbbyyOCRImage. Is a representation of an OCRImage suitable for
@@ -39,7 +38,7 @@ public class AbbyyOCRImage extends AbstractOCRImage implements OCRImage {
 
 	/** This represents the URI to the remote system. */
 	private URI remoteUri;
-	
+
 	/** This represents the URI if the images is part of a failed process. */
 	private URI errorUri;
 
@@ -59,14 +58,21 @@ public class AbbyyOCRImage extends AbstractOCRImage implements OCRImage {
 
 	/**
 	 * Instantiates a new AbbyyOCRImage.
-	 *
-	 * @param uri The URI of the local file,
-	 * @param orientation {@link Orientation}
-	 * @param remoteUri, The URI of the file, need to be resolvable from the local machine.
-	 * @param remoteFileName, The local file name on the remote system.
-	 * @param size the size of the file
+	 * 
+	 * @param uri
+	 *            The URI of the local file,
+	 * @param orientation
+	 *            {@link Orientation}
+	 * @param remoteUri
+	 *            , The URI of the file, need to be resolvable from the local
+	 *            machine.
+	 * @param remoteFileName
+	 *            , The local file name on the remote system.
+	 * @param size
+	 *            the size of the file
 	 */
-	public AbbyyOCRImage(URI uri, Orientation orientation, URI remoteUri, String remoteFileName, Long size) {
+	public AbbyyOCRImage(URI uri, Orientation orientation, URI remoteUri,
+			String remoteFileName, Long size) {
 		this.imageUri = uri;
 		this.orientation = orientation;
 		this.remoteUri = remoteUri;
@@ -74,20 +80,22 @@ public class AbbyyOCRImage extends AbstractOCRImage implements OCRImage {
 		this.size = size;
 	}
 
-	//This calls a copy constructor
+	// This calls a copy constructor
 	/**
 	 * Instantiates a new AbbyyOCRImage.
-	 *
-	 * @param i the OCRImage. This represents the a single
-	 *            image file to be recognized. Images are referenced by URI.
-	 *            {@link Orientation} is an Enum representing different possible
-	 *            orientations of an image.
+	 * 
+	 * @param i
+	 *            the OCRImage. This represents the a single image file to be
+	 *            recognized. Images are referenced by URI. {@link Orientation}
+	 *            is an Enum representing different possible orientations of an
+	 *            image.
 	 */
 	public AbbyyOCRImage(OCRImage i) {
 		super(i);
 	}
+
 	/**
-	 * Instantiates a new AbbyyOCRImage from a given {@link AbbyyOCRImage}. 
+	 * Instantiates a new AbbyyOCRImage from a given {@link AbbyyOCRImage}.
 	 * 
 	 * @param i
 	 *            the i
@@ -105,10 +113,14 @@ public class AbbyyOCRImage extends AbstractOCRImage implements OCRImage {
 
 	/**
 	 * Instantiates a new AbbyyOCRImage.
-	 *
-	 * @param imageUri The URI of the local file
-	 * @param remoteUri, The URI of the file, need to be resolvable from the local machine.
-	 * @param remoteFileName, The local file name on the remote system.
+	 * 
+	 * @param imageUri
+	 *            The URI of the local file
+	 * @param remoteUri
+	 *            , The URI of the file, need to be resolvable from the local
+	 *            machine.
+	 * @param remoteFileName
+	 *            , The local file name on the remote system.
 	 */
 	public AbbyyOCRImage(URI imageUri, URI remoteUri, String remoteFileName) {
 		super(imageUri);
@@ -119,66 +131,78 @@ public class AbbyyOCRImage extends AbstractOCRImage implements OCRImage {
 
 	/**
 	 * Gets the remoteFilename is the local file name on the remote system.
+	 * 
 	 * @return the remoteFilename.
 	 */
-	public String getRemoteFileName () {
+	public String getRemoteFileName() {
 		return remoteFileName;
 	}
 
 	/**
 	 * Sets the remoteFilename is the local file name on the remote system.
+	 * 
 	 * @param remoteFileName
 	 */
-	public void setRemoteFileName (String remoteFileName) {
+	public void setRemoteFileName(String remoteFileName) {
 		this.remoteFileName = remoteFileName;
 	}
 
 	/**
-	 * Gets the remoteUri. The URI of the file, need to be resolvable from the local machine.
+	 * Gets the remoteUri. The URI of the file, need to be resolvable from the
+	 * local machine.
+	 * 
 	 * @return the remoteUri
 	 */
-	public URI getRemoteUri () {
+	public URI getRemoteUri() {
 		return this.remoteUri;
 	}
 
 	/**
-	 * Sets the remoteUri. The URI of the file, need to be resolvable from the local machine.
-	 * @param remoteUri 
+	 * Sets the remoteUri. The URI of the file, need to be resolvable from the
+	 * local machine.
+	 * 
+	 * @param remoteUri
 	 */
-	public void setRemoteUri (URI remoteUri) {
+	public void setRemoteUri(URI remoteUri) {
 		this.remoteUri = remoteUri;
 	}
 
 	/**
 	 * Gets the size of file
+	 * 
 	 * @return the size
 	 */
-	public Long getSize () {
+	public Long getSize() {
 		return size;
 	}
 
 	/**
 	 * Sets the size of file
-	 * @param size 
+	 * 
+	 * @param size
 	 */
-	public void setSize (Long size) {
+	public void setSize(Long size) {
 		this.size = size;
 	}
 
 	/**
-	 * Gets the errorUri, This represents the URI if the images is part of a failed process.
+	 * Gets the errorUri, This represents the URI if the images is part of a
+	 * failed process.
+	 * 
 	 * @return the errorUri
 	 */
-	public URI getErrorUri () {
+	public URI getErrorUri() {
 		return errorUri;
 	}
 
 	/**
-	 * Sets the errorUri, This represents the URI if the images is part of a failed process.
+	 * Sets the errorUri, This represents the URI if the images is part of a
+	 * failed process.
+	 * 
 	 * @param errorUri
 	 */
-	public void setErrorUri (URI errorUri) {
+	public void setErrorUri(URI errorUri) {
 		this.errorUri = errorUri;
 	}
-	
+
 }
