25992fd507950c6e2a4763e71de040e5f4b27237
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2011-01-21 14:41:53 +0000

    Modified and Completed

--- ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/metadata/SerializerTextMD.java
+++ ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/metadata/SerializerTextMD.java
@@ -2,50 +2,50 @@ package de.uni_goettingen.sub.commons.ocr.metadata;
 
 /*
 
-© 2010, SUB Goettingen. All rights reserved.
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
+ © 2010, SUB Goettingen. All rights reserved.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Affero General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Affero General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-*/
+ */
 
 import java.io.File;
 import java.io.OutputStream;
 
-
-
 /**
  * The Interface SerializerTextMD can be used to obtain a description of the.
  * 
- * {@link OCRProcessMetadata} and it's results This can be used to Creating a TextMD,
- * there are two methods which can provide textMD
+ * {@link OCRProcessMetadata} and it's results This can be used to Creating a
+ * TextMD, there are two methods which can provide textMD
  * 
  * @author abergna
  */
 
 public interface SerializerTextMD {
-		
+
 	/**
-	 * Write.
-	 *
-	 * @param file the file
+	 * Write. This can be used to Creating a TextMD
+	 * 
+	 * @param file
+	 *            the file
 	 */
-	abstract public void write(File file);	
-	
+	abstract public void write(File file);
+
 	/**
-	 * Write.
-	 *
-	 * @param outputstream the outputstream
+	 * Write. This can be used to Creating a TextMD
+	 * 
+	 * @param outputstream
+	 *            the outputstream
 	 */
 	abstract public void write(OutputStream outputstream);
-	
+
 }
