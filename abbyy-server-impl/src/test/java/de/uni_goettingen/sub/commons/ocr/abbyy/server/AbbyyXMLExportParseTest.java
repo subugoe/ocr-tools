785b0403f5a24cec8fc7287fb1b057427c1e47f0
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2011-01-21 13:53:32 +0000

    added License and removed geBasfolderAsfile method

--- abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/AbbyyXMLExportParseTest.java
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/AbbyyXMLExportParseTest.java
@@ -1,21 +1,30 @@
 package de.uni_goettingen.sub.commons.ocr.abbyy.server;
 
+/*
+
+ © 2010, SUB Göttingen. All rights reserved.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 import static org.junit.Assert.assertTrue;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Scanner;
 
-import org.apache.log4j.helpers.Loader;
 import org.apache.xmlbeans.XmlException;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,64 +39,47 @@ public class AbbyyXMLExportParseTest {
 	protected static DocumentDocument documentDocument;
 	Document doc;
 	private static InputStream isDoc, isDocString;
-	final static Logger logger = LoggerFactory.getLogger(AbbyyXMLExportParseTest.class);
-	
-	//public static final String NAMESPACE = "http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml";
-	
-	
-	
+	final static Logger logger = LoggerFactory
+			.getLogger(AbbyyXMLExportParseTest.class);
+
+	// public static final String NAMESPACE =
+	// "http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml";
+
 	@Before
-	public void init() throws XmlException, IOException{
-		 
-		
-		File filexmlExport = getBaseFolderAsFile();
-		filexmlExport = new File(System.getProperty("user.dir") + File.separator +"src/test/resources/hotfolder/"+ "xmlExport.xml");				
+	public void init() throws XmlException, IOException {
+
+		File filexmlExport = new File(AbbyyTicketTest.BASEFOLDER_FILE
+				+ "/hotfolder/" + "xmlExport.xml");
 		isDoc = new FileInputStream(filexmlExport);
 		isDocString = new FileInputStream(filexmlExport);
 		documentDocument = DocumentDocument.Factory.parse(isDoc);
 		doc = documentDocument.getDocument();
 
 	}
-	
+
 	@Test
-	public void getSoftwareNameAndVersion() throws IOException{
+	public void getSoftwareNameAndVersion() throws IOException {
 		String xmlexport = doc.getProducer();
 		String[] splittArray = xmlexport.split(" ");
-		assertTrue( splittArray[0].equals("FineReader"));
+		assertTrue(splittArray[0].equals("FineReader"));
 		assertTrue(splittArray[1].equals("8.0"));
-		
-	}
-	
-	@Test
-	public void getDocumentType(){
-		String xmlexport = doc.toString();
-		String[] splittArray1 , documentType;
-		splittArray1 = xmlexport.split("schemaLocation=");
-		documentType = splittArray1[1].split(" ");
-		System.out.println("Value: " + documentType[0].substring(1));
-		assertTrue((documentType[0].substring(1)).equals("http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml"));
-	}
-	
-	@Test
-	public void getProcessingNote() throws IOException{		
-		String document = doc.toString();
-		assertTrue(document != null);
-		System.out.println(doc.toString());
 
 	}
-	
-	
-	public static File getBaseFolderAsFile() {
-		File basefolder;
-		// TODO: GDZ: Do wee really need to depend on Log4J here? I don't think
-		// so...
-		URL url = Loader.getResource("");
-		try {
-			basefolder = new File(url.toURI());
-		} catch (URISyntaxException ue) {
-			basefolder = new File(url.getPath());
-		}
-		return basefolder;
+
+	@Test
+	public void getDocumentType() {
+		String xmlexport = doc.toString();
+		String[] splittArray1, documentType;
+		splittArray1 = xmlexport.split("schemaLocation=");
+		documentType = splittArray1[1].split(" ");
+		assertTrue((documentType[0].substring(1))
+				.equals("http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml"));
 	}
-	
+
+	@Test
+	public void getProcessingNote() throws IOException {
+		String document = doc.toString();
+		assertTrue(document != null);
+	}
+
 }
