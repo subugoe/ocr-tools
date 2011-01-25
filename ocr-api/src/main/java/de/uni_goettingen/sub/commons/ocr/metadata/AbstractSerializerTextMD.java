8e711465d411d928c58ce0a293c2325142e7af81
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2011-01-25 11:39:29 +0000

    added LANGUAGE_MAP for TextMD

--- ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/metadata/AbstractSerializerTextMD.java
+++ ocr-api/src/main/java/de/uni_goettingen/sub/commons/ocr/metadata/AbstractSerializerTextMD.java
@@ -24,6 +24,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import noNamespace.TextMDDocument;
@@ -54,6 +55,11 @@ import de.uni_goettingen.sub.commons.ocr.api.OCRProcessMetadata;
  * 
  */
 public abstract class AbstractSerializerTextMD implements SerializerTextMD {
+	/**
+	 * This Map contains the mapping from java.util.Locale to the Strings needed
+	 * by TextMD
+	 */
+	public final static Map<Locale, String> LANGUAGE_MAP = new HashMap<Locale, String>();
 
 	/** The tmpfiles. */
 	protected Map<String, File> tmpfiles = new HashMap<String, File>();
@@ -75,6 +81,124 @@ public abstract class AbstractSerializerTextMD implements SerializerTextMD {
 	// ByteSize in element character_info
 	ByteSize bs;
 	CharacterSize cs;
+	
+	static{
+		/**
+		 * Language(s) used in work.  Use ISO 639-2 codes
+		*/
+		// only Abbyy Recognition Languages
+		LANGUAGE_MAP.put(new Locale("ab"), "abk");			LANGUAGE_MAP.put(new Locale("az"), "aze");
+		LANGUAGE_MAP.put(new Locale("af"), "afr"); 			LANGUAGE_MAP.put(new Locale("am"), "amh");
+		LANGUAGE_MAP.put(new Locale("ay"), "aym"); 	
+		/*LANGUAGE_MAP.put(new Locale("ar"), "ara"); 		LANGUAGE_MAP.put(new Locale("as"), "asm");
+		LANGUAGE_MAP.put(new Locale("aa"), "aar"); */
+		
+		
+		LANGUAGE_MAP.put(new Locale("ba"), "bak"); 			LANGUAGE_MAP.put(new Locale("be"), "bel");
+		LANGUAGE_MAP.put(new Locale("bg"), "bul");			LANGUAGE_MAP.put(new Locale("br"), "bre");
+		/*LANGUAGE_MAP.put(new Locale("bo"), "tib/bod"); 	LANGUAGE_MAP.put(new Locale("bh"), "bih");
+		LANGUAGE_MAP.put(new Locale("bi"), "bis"); 			LANGUAGE_MAP.put(new Locale("bn"), "ben");*/
+		
+		LANGUAGE_MAP.put(new Locale("ca"), "cat"); 			LANGUAGE_MAP.put(new Locale("co"), "cos");
+		LANGUAGE_MAP.put(new Locale("cs"), "cze/ces"); 		LANGUAGE_MAP.put(new Locale("cy"), "wel/cym");
+		
+		LANGUAGE_MAP.put(new Locale("da"), "dan"); 			LANGUAGE_MAP.put(Locale.GERMAN, "ger/deu");
+		/*LANGUAGE_MAP.put(new Locale("dz"), "Bhutani");*/
+		
+		LANGUAGE_MAP.put(new Locale("el"), "gre/ell");		LANGUAGE_MAP.put(Locale.ENGLISH, "eng");
+		LANGUAGE_MAP.put(new Locale("es"), "spa");      LANGUAGE_MAP.put(new Locale("et"), "est"); 	
+		LANGUAGE_MAP.put(new Locale("eu"), "Basque");
+		/*LANGUAGE_MAP.put(new Locale("eo"), "epo");*/ 
+		
+		LANGUAGE_MAP.put(new Locale("fi"), "fin");      LANGUAGE_MAP.put(new Locale("fo"), "fao");
+		LANGUAGE_MAP.put(new Locale("fr"), "fre/fra"); 		LANGUAGE_MAP.put(new Locale("fy"), "fry");
+		/*LANGUAGE_MAP.put(new Locale("fa"), "per/fas");  LANGUAGE_MAP.put(new Locale("fj"), "fij"); */
+		
+		LANGUAGE_MAP.put(new Locale("ga"), "gle");		LANGUAGE_MAP.put(new Locale("gd"), "gla");
+		LANGUAGE_MAP.put(new Locale("gl"), "glg "); 	LANGUAGE_MAP.put(new Locale("gn"), "grn");
+		LANGUAGE_MAP.put(new Locale("gu"), "guj"); 	
+		
+		LANGUAGE_MAP.put(new Locale("ha"), "hau");		LANGUAGE_MAP.put(new Locale("he"), "heb");
+		LANGUAGE_MAP.put(new Locale("hr"), "scr/hrv");		LANGUAGE_MAP.put(new Locale("hy"), "arm/hye");
+		LANGUAGE_MAP.put(new Locale("hu"), "Hungarian"); 	
+		/*LANGUAGE_MAP.put(new Locale("hi"), "hin"); */
+		
+		LANGUAGE_MAP.put(new Locale("id"), "ind"); 	LANGUAGE_MAP.put(new Locale("it"), "ita");
+		/*LANGUAGE_MAP.put(new Locale("ie"), "ile"); 	LANGUAGE_MAP.put(new Locale("ik"), "ipk");
+		LANGUAGE_MAP.put(new Locale("is"), "ice/isl"); 	 LANGUAGE_MAP.put(new Locale("ia"), "ina");
+		LANGUAGE_MAP.put(new Locale("iu"), "iku");  */ 
+		
+		LANGUAGE_MAP.put(new Locale("ja"), "jpn");		
+		/*LANGUAGE_MAP.put(new Locale("jw"), "jav");*/
+		
+		LANGUAGE_MAP.put(new Locale("ko"), "kor");		LANGUAGE_MAP.put(new Locale("ku"), "kur");
+		LANGUAGE_MAP.put(new Locale("ky"), "kir"); 		LANGUAGE_MAP.put(new Locale("kk"), "kaz");
+		/*LANGUAGE_MAP.put(new Locale("ka"), "geo/kat");		LANGUAGE_MAP.put(new Locale("kn"), "kan"); 		
+		LANGUAGE_MAP.put(new Locale("km"), "Cambodian"); 		LANGUAGE_MAP.put(new Locale("ks"), "kas");*/
+		
+		LANGUAGE_MAP.put(new Locale("la"), "lat"); 		LANGUAGE_MAP.put(new Locale("lt"), "lit");	
+		LANGUAGE_MAP.put(new Locale("lv"), "lav");
+		/*LANGUAGE_MAP.put(new Locale("ln"), "lin");  LANGUAGE_MAP.put(new Locale("lo"), "lao");	*/
+	 	
+		
+		LANGUAGE_MAP.put(new Locale("mg"), "mlg");		LANGUAGE_MAP.put(new Locale("mi"), "mao/mri");
+		LANGUAGE_MAP.put(new Locale("mk"), "mac/mkd"); 	LANGUAGE_MAP.put(new Locale("ms"), "may/msa");	
+		LANGUAGE_MAP.put(new Locale("mn"), "mon"); 	LANGUAGE_MAP.put(new Locale("mo"), "mol");
+		LANGUAGE_MAP.put(new Locale("mt"), "mlt"); 		
+		/*LANGUAGE_MAP.put(new Locale("my"), "bur/mya"); LANGUAGE_MAP.put(new Locale("ml"), "mal");
+		LANGUAGE_MAP.put(new Locale("mr"), "mar");
+		*/
+		
+		LANGUAGE_MAP.put(new Locale("nl"), "dut/nld"); 		LANGUAGE_MAP.put(new Locale("no"), "nor");
+		/*LANGUAGE_MAP.put(new Locale("na"), "nau");		LANGUAGE_MAP.put(new Locale("ne"), "nep");*/
+	
+			LANGUAGE_MAP.put(new Locale("oc"), "oci");		
+		/*LANGUAGE_MAP.put(new Locale("om"), "orm");LANGUAGE_MAP.put(new Locale("or"), "ori"); */
+		
+		LANGUAGE_MAP.put(new Locale("pl"), "pol");		LANGUAGE_MAP.put(new Locale("pt"), "por");
+		/*LANGUAGE_MAP.put(new Locale("pa"), "Punjabi");	LANGUAGE_MAP.put(new Locale("ps"), "Pashto"); */	
+		
+		LANGUAGE_MAP.put(new Locale("qu"), "que");
+		
+		LANGUAGE_MAP.put(new Locale("rm"),"roh");  LANGUAGE_MAP.put(new Locale("ru"), "rus");
+		LANGUAGE_MAP.put(new Locale("ro"), "rum/ron");		
+		/*LANGUAGE_MAP.put(new Locale("rw"), "kin"); LANGUAGE_MAP.put(new Locale("rn"), "Kirundi");*/
+		
+		LANGUAGE_MAP.put(new Locale("sk"), "slo/slk");		LANGUAGE_MAP.put(new Locale("sv"), "swe");
+		LANGUAGE_MAP.put(new Locale("sl"), "slv");	LANGUAGE_MAP.put(new Locale("sm"), "smo");
+		LANGUAGE_MAP.put(new Locale("sn"), "sna"); 		LANGUAGE_MAP.put(new Locale("so"), "som");
+		LANGUAGE_MAP.put(new Locale("sq"), "alb/sqi"); 	LANGUAGE_MAP.put(new Locale("sr"), "scc/srp");
+		LANGUAGE_MAP.put(new Locale("sw"), "swa");
+		/*LANGUAGE_MAP.put(new Locale("sa"), "san");		LANGUAGE_MAP.put(new Locale("sd"), "snd");
+		LANGUAGE_MAP.put(new Locale("sg"), "sag"); 		LANGUAGE_MAP.put(new Locale("sh"), "Serbo-Croatian");
+		LANGUAGE_MAP.put(new Locale("si"), "sin");	LANGUAGE_MAP.put(new Locale("su"), "sun");
+		LANGUAGE_MAP.put(new Locale("ss"), "Siswati");		LANGUAGE_MAP.put(new Locale("st"), "Sesotho");*/
+					
+		LANGUAGE_MAP.put(new Locale("tg"), "tgk"); 		LANGUAGE_MAP.put(new Locale("th"), "tha");
+		LANGUAGE_MAP.put(new Locale("tk"), "tuk");		LANGUAGE_MAP.put(new Locale("tl"), "tgl");	
+		LANGUAGE_MAP.put(new Locale("to"), "tog"); 		LANGUAGE_MAP.put(new Locale("tr"), "tur");
+		LANGUAGE_MAP.put(new Locale("tt"), "Tatar");
+		/*LANGUAGE_MAP.put(new Locale("ts"), "tso"); 		LANGUAGE_MAP.put(new Locale("ti"), "tir"); 	  		
+		LANGUAGE_MAP.put(new Locale("tn"), "Setswana");		LANGUAGE_MAP.put(new Locale("tw"), "twi");
+		LANGUAGE_MAP.put(new Locale("ta"), "tam");		LANGUAGE_MAP.put(new Locale("te"), "tel");*/
+	
+		LANGUAGE_MAP.put(new Locale("ug"), "uig");		LANGUAGE_MAP.put(new Locale("uk"), "ukr");
+		LANGUAGE_MAP.put(new Locale("uz"), "uzb");
+		/*LANGUAGE_MAP.put(new Locale("ur"), "urd");*/ 	
+		
+		/*LANGUAGE_MAP.put(new Locale("vi"), "vie"); 	LANGUAGE_MAP.put(new Locale("vo"), "vol");*/
+		
+		LANGUAGE_MAP.put(new Locale("wo"), "wol");
+		
+		LANGUAGE_MAP.put(new Locale("xh"), "xho");
+		
+		LANGUAGE_MAP.put(new Locale("yi"), "yid"); 		
+		/*LANGUAGE_MAP.put(new Locale("yo"), "yor");*/
+		
+		LANGUAGE_MAP.put(new Locale("zu"), "zul");
+		LANGUAGE_MAP.put(new Locale("zh"), "chi/zho");
+		/*LANGUAGE_MAP.put(new Locale("za"), "zha");	*/
+	}
 
 	/**
 	 * Instantiates a new AbstractSerializerTextMD.
@@ -181,16 +305,19 @@ public abstract class AbstractSerializerTextMD implements SerializerTextMD {
 
 		
 		// Languages
-		Language language = textMD.addNewLanguage();
+		/*Language language = textMD.addNewLanguage();
 		language.set(noNamespace.TextMDDocument.TextMD.Language.Enum
-				.forString("alb/sqi"));
-		/*
-		 * if (ocrProcessMetadata.getLanguages() != null){ for (Locale l :
-		 * ocrProcessMetadata.getLanguages()){ Language language =
-		 * textMD.addNewLanguage(); language.set("BAN"); }
-		 * 
-		 * }
-		 */
+				.forString("alb/sqi"));*/
+		
+		if (ocrProcessMetadata.getLanguages() != null) {
+			for (Locale l : ocrProcessMetadata.getLanguages()) {
+				Language language = textMD.addNewLanguage();
+				language.set(noNamespace.TextMDDocument.TextMD.Language.Enum
+						.forString(LANGUAGE_MAP.get(l)));
+			}
+
+		}
+		 
 
 		// AltLanguage
 		AltLanguage altlanguage = textMD.addNewAltLanguage();
