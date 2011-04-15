d9faa6d1c049f5b92754684aa128bd40ccb8aea9
Dennis Neumann <neumann@sub.uni-goettingen.de>
2011-04-15 10:08:16 +0000

    ocr von einzelnen Bildern ist möglich

--- tesseract-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/tesseract/TesseractOCRProcess.java
+++ tesseract-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/tesseract/TesseractOCRProcess.java
@@ -1,28 +1,41 @@
 package de.uni_goettingen.sub.commons.ocr.tesseract;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Locale;
 
 import de.uni_goettingen.sub.commons.ocr.api.AbstractOCRProcess;
 import de.uni_goettingen.sub.commons.ocr.api.OCRFormat;
+import de.uni_goettingen.sub.commons.ocr.api.OCRImage;
 import de.uni_goettingen.sub.commons.ocr.api.OCROutput;
 import de.uni_goettingen.sub.commons.ocr.api.OCRProcess;
 
 public class TesseractOCRProcess extends AbstractOCRProcess implements
 		OCRProcess {
 
+	private static Map<String, String> languages = new HashMap<String, String>();
 	
-	public TesseractOCRProcess(OCRProcess process){
+	static {
+		languages.put("german", "deu");
+		languages.put("english", "eng");
+	}
+	
+	public TesseractOCRProcess(OCRProcess process) {
 		super(process);
 	}
-	public TesseractOCRProcess(){
+
+	public TesseractOCRProcess() {
 		super();
 	}
-	
+
 	@Override
 	public void addOutput(OCRFormat format, OCROutput output) {
-		
+
 		if (ocrOutputs == null) {
 			// We use a LinkedHashMap to get the order of the elements
 			// predictable
@@ -31,5 +44,36 @@ public class TesseractOCRProcess extends AbstractOCRProcess implements
 		ocrOutputs.put(format, output);
 	}
 
-	
+	public void start() {
+		for (Map.Entry<OCRFormat, OCROutput> formatToOutput : ocrOutputs
+				.entrySet()) {
+			for (OCRImage image : ocrImages) {
+
+				String imagePath = image.getUri().toString();
+				String outputPath = formatToOutput.getValue().getUri()
+						.toString();
+				if (imagePath.startsWith("file:")
+						&& outputPath.startsWith("file:")) {
+					File inputImage = new File(imagePath.substring(5));
+					File output = new File(outputPath.substring(5));
+					Locale locale = new ArrayList<Locale>(langs).get(0);
+					OCRFormat format = formatToOutput.getKey();
+
+					Tesseract tesseract = new Tesseract(inputImage, output);
+					tesseract.setFormat(format);
+					tesseract.setLanguage( languages.get(locale.getLanguage()) );
+					
+					if(getTextTyp() == OCRTextTyp.Gothic) {
+						tesseract.setGothic(true);
+					}
+					
+					tesseract.execute();
+				} else {
+					System.out.println("--------------- error");
+				}
+
+			}
+		}
+	}
+
 }
