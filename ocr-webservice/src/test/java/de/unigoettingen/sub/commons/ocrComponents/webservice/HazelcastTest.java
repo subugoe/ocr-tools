62a31da923dc33548bbf78152cdef8010cec3cd1
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2011-06-01 14:24:04 +0000

    Priority und synchronisation

--- ocr-webservice/src/test/java/de/unigoettingen/sub/commons/ocrComponents/webservice/HazelcastTest.java
+++ ocr-webservice/src/test/java/de/unigoettingen/sub/commons/ocrComponents/webservice/HazelcastTest.java
@@ -3,28 +3,39 @@ package de.unigoettingen.sub.commons.ocrComponents.webservice;
 
 import static java.lang.Thread.sleep;
 
-import java.io.Serializable;
+
 import java.util.Comparator;
 import java.util.Date;
+import java.util.LinkedList;
+
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 
 import javax.swing.JOptionPane;
 
+
+
+
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+
 import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
-import com.hazelcast.core.IMap;
-import com.hazelcast.core.IQueue;
+
+
+
+
 
 
 public class HazelcastTest {
-
+	final static Logger logger = LoggerFactory.getLogger(HazelcastTest.class);
+	
 	static final Comparator<Item> ORDER = new ItemComparator();
 
 
@@ -36,31 +47,61 @@ public class HazelcastTest {
 	@Test
 	public void testHazelcast() throws InterruptedException{
 		System.out.println("------------test1");
-		 HazelcastInstance h = Hazelcast.newHazelcastInstance(null);
-         IList<Item> temp = h.getList("default");
-         
-         temp.add(new Item(2, new Date().getTime()));
-         temp.add(new Item(1, new Date().getTime()));
-         System.out.println("+++++++++++"+ temp.size());
-         while (temp.size() < 3) {
-        	 temp = h.getList("default");
-         }
-         
+		
+		HazelcastInstance h = Hazelcast.newHazelcastInstance(null);
+
          PriorityQueue<Item> q = new PriorityQueue<Item>(100, ORDER);
-         q.addAll(temp);
+         BlockingQueue<Item> queue = h.getQueue("default");
+         Queue<Item> listqueue = new LinkedList<Item>(); 
+         for(int i=1 ; i < 5; i++){
+        	sleep(2000); 
+        	Item a = new Item(1, new Date().getTime(), "A" +i );
+            listqueue.add(a);
+            queue.add(a);
+            sleep(10);
+            a = new Item(2, new Date().getTime(), "B" +i);
+            listqueue.add(a);
+            queue.add(a);
+            sleep(10);
+            a = new Item(1, new Date().getTime(), "C"+i);
+            listqueue.add(a);
+            queue.add(a);
+            sleep(10);
+            a = new Item(4, new Date().getTime(), "D"+i );
+            listqueue.add(a);
+            queue.add(a);
+            sleep(10);
+            a = new Item(5, new Date().getTime(), "E" +i);
+            listqueue.add(a);
+            queue.add(a);     
+         } 
          
+         JOptionPane.showMessageDialog(null, "Start Hazelcast");
+         q.addAll(queue);
          
-         Item i = q.poll();
-         System.out.println(i.getPrio() + " " + i.getTime());
-         i = q.poll();
-         System.out.println(i.getPrio() + " " + i.getTime());
-         i = q.poll();
-         System.out.println(i.getPrio() + " " + i.getTime());
-        
+        // sleep(1500);
+         System.out.println("++++++ A Start PriorityQueue SIZE +++++ "+ q.size());
+         while(listqueue.size() > 0){
+        	 Item i = q.poll();
+        	 if(i != null){
+        		 if(i.getValue().equals("A1") || i.getValue().equals("B1")|| i.getValue().equals("C1")|| i.getValue().equals("D1")|| i.getValue().equals("E1")|| 
+        				 i.getValue().equals("A2") || i.getValue().equals("B2")|| i.getValue().equals("C2")|| i.getValue().equals("D2")|| i.getValue().equals("E2") ||
+        				 i.getValue().equals("A3") || i.getValue().equals("B3")|| i.getValue().equals("C3")|| i.getValue().equals("D3")|| i.getValue().equals("E3")|| 
+        				 i.getValue().equals("A4") || i.getValue().equals("B4")|| i.getValue().equals("C4")|| i.getValue().equals("D4")|| i.getValue().equals("E4")){
+      				sleep(300);
+      				System.out.println("#### A SORTED #### " + i.getPrio() + " "
+      						+ i.getTime() + " " + i.getValue());
+      				listqueue.poll();
+      				//queue.remove(i);
+             	}
+        	 }	
+         }
+        System.out.println("++++++ A Q Prioritie +++++ "+ q.size());
+ 		System.out.println("++++++ A QUEUE Hazelcast+++++ "+ queue.size());
  	}
 	
 	@After
 	public void tearDown() throws Exception {
+		//Hazelcast.shutdownAll();
 	}
-
 }
--- ocr-webservice/src/test/java/de/unigoettingen/sub/commons/ocrComponents/webservice/ZazelcastTest.java
+++ ocr-webservice/src/test/java/de/unigoettingen/sub/commons/ocrComponents/webservice/ZazelcastTest.java
@@ -1,28 +1,32 @@
 package de.unigoettingen.sub.commons.ocrComponents.webservice;
 
 
-import java.io.Serializable;
+
 import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
+import java.util.LinkedList;
+
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+
 
 import javax.swing.JOptionPane;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
-import com.hazelcast.core.IMap;
-import com.hazelcast.core.IQueue;
+
+
 import static java.lang.Thread.sleep;
 
 public class ZazelcastTest {
-
+	final static Logger logger = LoggerFactory.getLogger(HazelcastTest.class);
 	static final Comparator<Item> ORDER = new ItemComparator();
 
 
@@ -35,17 +39,62 @@ public class ZazelcastTest {
 	public void testHazelcast() throws InterruptedException{
 		System.out.println("------------test2");
 
-		 HazelcastInstance h = Hazelcast.newHazelcastInstance(null);
-         IList<Item> temp = h.getList("default");
-         
-         temp.add(new Item(2, new Date().getTime()));
- 
-         
-         sleep(5000);
- 	}
+		HazelcastInstance h = Hazelcast.newHazelcastInstance(null);
+	
+        PriorityQueue<Item> q = new PriorityQueue<Item>(100, ORDER);
+        BlockingQueue<Item> queue = h.getQueue("default");
+        Queue<Item> listqueue = new LinkedList<Item>(); 
+
+        for(int i=1 ; i < 5; i++){
+        	 sleep(2000);
+        	 Item a = new Item(1, new Date().getTime(), "I" +i );
+             listqueue.offer(a);
+             queue.add(a);
+             sleep(10);
+             a = new Item(2, new Date().getTime(), "J"+i );
+             listqueue.offer(a);
+             queue.add(a);
+             sleep(10);
+             a = new Item(1, new Date().getTime(), "K"+i);
+             listqueue.offer(a);
+             queue.add(a);
+             sleep(10);
+             a = new Item(4, new Date().getTime(), "L"+i );
+             listqueue.offer(a);
+             queue.add(a);
+             sleep(10);
+             a = new Item(5, new Date().getTime(), "M" +i);
+             listqueue.offer(a);
+             queue.add(a);       	
+        }
+            		
+        JOptionPane.showMessageDialog(null, "Start Hazelcast");
+		q.addAll(queue);
+		//sleep(500);
+		
+		System.out.println("++++++ B Start PriorityQueue SIZE +++++ "+ q.size());
+
+		while (listqueue.size() > 0) {
+			Item i = q.poll();
+			if(i != null){
+				if(i.getValue().equals("I1") || i.getValue().equals("K1")|| i.getValue().equals("L1")|| i.getValue().equals("M1")|| i.getValue().equals("J1") || 
+						i.getValue().equals("I2") || i.getValue().equals("K2")|| i.getValue().equals("L2")|| i.getValue().equals("M2")|| i.getValue().equals("J2")||
+						i.getValue().equals("I3") || i.getValue().equals("K3")|| i.getValue().equals("L3")|| i.getValue().equals("M3")|| i.getValue().equals("J3")||
+						i.getValue().equals("I4") || i.getValue().equals("K4")|| i.getValue().equals("L4")|| i.getValue().equals("M4")|| i.getValue().equals("J4")){
+					sleep(300);
+					System.out.println("#### B SORTED #### " + i.getPrio() + " "
+							+ i.getTime() + " " + i.getValue());
+					listqueue.poll();
+					//queue.remove(i);				
+				}
+			}
+		}
+		System.out.println("++++++ B Q Prioritie +++++ "+ q.size());
+		System.out.println("++++++ B QUEUE Hazelcast+++++ "+ queue.size());
+	}
 	
 	@After
 	public void tearDown() throws Exception {
+		
 	}
-
 }
