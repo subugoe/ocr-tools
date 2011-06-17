97f05fcacdc50154f538aedf45dadb1fc7d34adf
Mohamed Abergna <abergna@sub.uni-goettingen.de>
2011-06-17 10:10:09 +0000

    added AbbyyServerOCRouput for test

---
+++ abbyy-server-impl/src/test/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/AbbyyServerOCRouput.java
@@ -0,0 +1,80 @@
+package de.uni_goettingen.sub.commons.ocr.abbyy.server;
+
+
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.uni_goettingen.sub.commons.ocr.abbyy.server.hotfolder.JackrabbitHotfolderImpl;
+import de.uni_goettingen.sub.commons.ocr.api.OCRFormat;
+
+public class AbbyyServerOCRouput implements Runnable  {
+	final static Logger logger = LoggerFactory.getLogger(AbbyyServerOCRouput.class);
+	static JackrabbitHotfolderImpl imp;
+	File filefrom = new File(AbbyyOCRProcessTest.BASEFOLDER_FILE.getAbsoluteFile()+ "/results");
+	//AbbyyServerOCRouput abbyyServerOCRouput= new AbbyyServerOCRouput();
+	
+	@Override
+	public void run() {
+		imp = new JackrabbitHotfolderImpl("http://localhost:8090/", "user","pw");
+			
+		try {
+			mkDir();
+			logger.debug("Wait for copy to out");
+			Thread.currentThread();
+			Thread.sleep(8000);
+			logger.debug("Starting to copy HOTFOLDER");
+				
+				copyToHotfolder();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+	
+	public void mkDir() throws IOException, URISyntaxException{
+		File HOTFOLDER_TEST = new File(AbbyyOCRProcessTest.BASEFOLDER_FILE.getAbsoluteFile()+ "/HOTFOLDER_TEST");
+		if(!new File(HOTFOLDER_TEST.toString().replace("file:/", "")+ "/"+ "input").exists()){
+			imp.mkDir(new URI("http://localhost:8090/input"));
+			assertTrue(new File(HOTFOLDER_TEST.toString().replace("file:/", "")+ "/"+ "input").exists());
+		}
+		if(!new File(HOTFOLDER_TEST.toString().replace("file:/", "")+ "/"+ "output").exists()){
+			imp.mkDir(new URI("http://localhost:8090/output"));
+			assertTrue(new File(HOTFOLDER_TEST.toString().replace("file:/", "")+ "/"+ "output").exists());
+		}
+		if(!new File(HOTFOLDER_TEST.toString().replace("file:/", "")+ "/"+ "error").exists()){
+			imp.mkDir(new URI("http://localhost:8090/error"));
+			assertTrue(new File(HOTFOLDER_TEST.toString().replace("file:/", "")+ "/"+ "error").exists());
+		}
+	
+	}
+	
+	public void  copyToHotfolder() throws IOException, URISyntaxException{
+
+		for (String book : AbbyyOCRProcessTest.TEST_FOLDERS){
+			for (OCRFormat f: AbbyyTicketTest.OUTPUT_DEFINITIONS.keySet()){
+				filefrom = new File(AbbyyOCRProcessTest.BASEFOLDER_FILE.getAbsoluteFile()+ "/results"+ "/" + book + "." + f.toString().toLowerCase());
+				URI from = filefrom.toURI();
+				imp.copyFile(from, new URI("http://localhost:8090/output"+ "/" + book + "." + f.toString().toLowerCase()));
+			}
+			filefrom = new File(AbbyyOCRProcessTest.BASEFOLDER_FILE.getAbsoluteFile()+ "/results"+ "/" + book + ".xml.result.xml" );
+			imp.copyFile(filefrom.toURI(), new URI("http://localhost:8090/output"+ "/" + book + ".xml.result.xml"));
+		}
+		
+	}
+}
