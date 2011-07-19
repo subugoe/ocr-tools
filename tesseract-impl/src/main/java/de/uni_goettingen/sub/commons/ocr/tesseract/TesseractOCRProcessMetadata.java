15987ffaf10a42126f8b2561df74b44f6043031b
Dennis Neumann <neumann@sub.uni-goettingen.de>
2011-07-19 15:58:14 +0000

    Anpassungen, damit tesseract ausgewählt werden kann. ZB: mvn -P tesseract package

---
+++ tesseract-impl/src/main/java/de/uni_goettingen/sub/commons/ocr/tesseract/TesseractOCRProcessMetadata.java
@@ -0,0 +1,8 @@
+package de.uni_goettingen.sub.commons.ocr.tesseract;
+
+import de.uni_goettingen.sub.commons.ocr.api.AbstractOCRProcessMetadata;
+
+public class TesseractOCRProcessMetadata extends AbstractOCRProcessMetadata {
+
+
+}
