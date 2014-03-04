a55abfa805351e30f8389b1ebe1c21422ae2c21f
Dennis Neumann <neumann@sub.uni-goettingen.de>
2014-03-04 13:43:44 +0100

    Unit testing the servlet (doPost method) by extending it and sneaking in a mock.

--- web-frontend/src/test/java/de/unigoettingen/sub/commons/ocr/web/OcrServletTestPart.java
+++ web-frontend/src/test/java/de/unigoettingen/sub/commons/ocr/web/OcrServletTestPart.java
@@ -1,59 +1,53 @@
 package de.unigoettingen.sub.commons.ocr.web;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.TextPage;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 
 public class OcrServletTestPart {
 
 	private static int jettyPort = TestSuiteForJspsAndServlets.jettyPort;
-	private static Server jetty;
+	private WebClient webClient;
+	private WebRequest request;
+	private List<NameValuePair> params;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		//startJetty();
 	}
-
-	
-	private static void startJetty() throws Exception {
-		jetty = new Server(jettyPort);
-		WebAppContext context = new WebAppContext();
-		context.setContextPath("/");
-        context.setWar("src/main/webapp");
-        context.setDescriptor("src/test/resources/fake-web.xml");
-        jetty.setHandler(context);
-		
-		jetty.start();
-	}
-
 	
 	@Before
 	public void setUp() throws Exception {
+		webClient = new WebClient();
+		request = new WebRequest(new URL("http://localhost:" + jettyPort + "/ocr-servlet-child"), HttpMethod.POST);
+		params = new ArrayList<NameValuePair>();
 	}
 
 	@Test
-	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		WebClient webClient = new WebClient();
-		WebRequest request = new WebRequest(
-				  new URL("http://localhost:" + jettyPort + "/ocr-servlet-test"), HttpMethod.POST);
-		HtmlPage page = webClient.getPage(request);
-		System.out.println(page.asText());
+	public void servletShouldStartOcr() throws Exception {
+		params.add(new NameValuePair("fakeValidationMessage", "OK"));
+		request.setRequestParameters(params);
+		TextPage page = webClient.getPage(request);
+		assertEquals("Forwarded to view: ocr-started.jsp", page.getContent());
+	}
+
+	@Test
+	public void servletShouldDenyOcr() throws Exception {
+		params.add(new NameValuePair("fakeValidationMessage", "Error"));
+		request.setRequestParameters(params);
+		TextPage page = webClient.getPage(request);
+		assertEquals("Forwarded to view: invalid-parameters.jsp", page.getContent());
 	}
 
 }
