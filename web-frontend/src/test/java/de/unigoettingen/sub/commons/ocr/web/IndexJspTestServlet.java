39cd0db3ea4dff2955b44d276e8508800704d9b6
Dennis Neumann <neumann@sub.uni-goettingen.de>
2014-03-06 11:41:56 +0100

    some code beautifyings

--- web-frontend/src/test/java/de/unigoettingen/sub/commons/ocr/web/IndexJspTestServlet.java
+++ web-frontend/src/test/java/de/unigoettingen/sub/commons/ocr/web/IndexJspTestServlet.java
@@ -10,21 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class IndexJspTestServlet extends HttpServlet {
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 
-	/**
-     * Default constructor. 
-     */
     public IndexJspTestServlet() {
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println(request.getParameter("inputFolder"));
