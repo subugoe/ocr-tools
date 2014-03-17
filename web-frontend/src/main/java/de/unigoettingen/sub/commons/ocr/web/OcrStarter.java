bc0ba3e116aadf886fbfb64c03c91d4e63a7ecd3
Dennis Neumann <neumann@sub.uni-goettingen.de>
2014-03-17 16:35:45 +0100

    ocr with abbyy server is possible

--- web-frontend/src/main/java/de/unigoettingen/sub/commons/ocr/web/OcrStarter.java
+++ web-frontend/src/main/java/de/unigoettingen/sub/commons/ocr/web/OcrStarter.java
@@ -1,33 +1,91 @@
 package de.unigoettingen.sub.commons.ocr.web;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import de.uni_goettingen.sub.commons.ocr.api.OCREngine;
+import de.uni_goettingen.sub.commons.ocr.api.OCREngineFactory;
+import de.uni_goettingen.sub.commons.ocr.api.OCRFormat;
+import de.uni_goettingen.sub.commons.ocr.api.OCRImage;
+import de.uni_goettingen.sub.commons.ocr.api.OCROutput;
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess;
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess.OCRPriority;
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess.OCRTextType;
+import de.unigoettingen.sub.commons.ocr.util.OCRUtil;
+
 public class OcrStarter implements Runnable {
 
-	private OcrParameters ocrParameters;
+	private OcrParameters param;
 
 	public void setParameters(OcrParameters newParameters) {
-		ocrParameters = newParameters;
+		param = newParameters;
 	}
 
 	public String checkParameters() {
-		System.out.println(ocrParameters.email);
+		System.out.println(param.email);
 		return "OK";
 	}
 
 	@Override
 	public void run() {
-		while(true) {
-			System.out.println("ocr started");
-			System.out.println(Thread.currentThread().getName());
-			System.out.println(ocrParameters.email);
-			System.out.println();
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		ApplicationContext ac = new ClassPathXmlApplicationContext("abbyy" + "-context.xml");
+		OCREngineFactory ocrEngineFactory = (OCREngineFactory) ac
+					.getBean("OCREngineFactory");
+
+		OCREngine engine = ocrEngineFactory.newOcrEngine();
+
+		System.out.println(param.inputFolder);
+		File mainFolder = new File(param.inputFolder);
+		File[] subFolders = mainFolder.listFiles();
+		for (File bookFolder : subFolders) {
+			OCRProcess process = engine.newOcrProcess();
+			process.setName(bookFolder.getName());
+			
+			List<OCRImage> bookImages = new ArrayList<OCRImage>();
+			for (File imageFile : OCRUtil.makeFileList(bookFolder, param.imageFormat)) {
+				OCRImage image = engine.newOcrImage(imageFile.toURI());
+				image.setSize(imageFile.length());
+				bookImages.add(image);
 			}
+			process.setOcrImages(bookImages);
+			process.setPriority(OCRPriority.NORMAL);
+			
+			Set<Locale> langs = new HashSet<Locale>();
+			for (String lang : param.languages) {
+				langs.add(new Locale(lang));
+			}
+			process.setLanguages(langs);
+			process.setTextType(OCRTextType.valueOf(param.textType));
+			
+			try {
+				for (String formatString : param.outputFormats) {
+					OCRFormat format = OCRFormat.parseOCRFormat(formatString);
+					OCROutput output = engine.newOcrOutput();
+					URI uri = new URI(new File(param.outputFolder).toURI()
+							+ bookFolder.getName()
+							+ "." + format.toString().toLowerCase());
+					output.setUri(uri);
+					output.setlocalOutput(new File(param.outputFolder).getAbsolutePath());
+					process.addOutput(format, output);
+				}
+			} catch(URISyntaxException e){
+				System.out.println("uri: " + e);
+			}
+			
+			engine.addOcrProcess(process);
 		}
 		
+
+		engine.recognize();
 	}
 
 
--- web-frontend/src/test/java/de/unigoettingen/sub/commons/ocr/web/OcrStarterTest.java
+++ web-frontend/src/test/java/de/unigoettingen/sub/commons/ocr/web/OcrStarterTest.java
@@ -2,6 +2,8 @@ package de.unigoettingen.sub.commons.ocr.web;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,20 +16,11 @@ public class OcrStarterTest {
 	@Test
 	public void test() throws InterruptedException {
 		OcrStarter ocrStarter = new OcrStarter();
-		OcrStarter ocrStarter2 = new OcrStarter();
 		OcrParameters par = new OcrParameters();
-		par.email = "mail1";
+		par.inputFolder = new File("src/test/resources/testInputs").getAbsolutePath();
 		ocrStarter.setParameters(par);
-		OcrParameters par2 = new OcrParameters();
-		par2.email = "mail2";
 		
-		new Thread(ocrStarter).start();
-		Thread.sleep(7000);
-		ocrStarter.setParameters(par2);
-		new Thread(ocrStarter).start();
-		
-		Thread.sleep(1000000);
-		
+		ocrStarter.run();
 	}
 
 }
