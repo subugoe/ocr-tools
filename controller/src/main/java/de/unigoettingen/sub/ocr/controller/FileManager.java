cdfb55aebd6c67ad1d2c091a04f3155ee8bca255
Dennis Neumann <neumann@sub.uni-goettingen.de>
2014-06-10 14:28:07 +0200

    some implementation in controller

--- controller/src/main/java/de/unigoettingen/sub/ocr/controller/FileManager.java
+++ controller/src/main/java/de/unigoettingen/sub/ocr/controller/FileManager.java
@@ -1,6 +1,8 @@
 package de.unigoettingen.sub.ocr.controller;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
 
@@ -12,6 +14,46 @@ public class FileManager {
 	public boolean isWritableFolder(String outputFolder) {
 		File folder = new File(outputFolder);
 		return folder.exists() && folder.isDirectory() && folder.canWrite();
+	}
+
+	public File[] getAllFolders(String parentFolder, String[] imageTypes) {
+		File parent = new File(parentFolder);
+		List<File> imagesFromFolder = getImagesFromFolder(parent, imageTypes);
+		if (!imagesFromFolder.isEmpty()) {
+			return new File[]{parent};
+		} else {
+			List<File> folders = new ArrayList<File>();
+			File[] files = parent.listFiles();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					folders.add(file);
+				}
+			}
+			return folders.toArray(new File[]{});
+		}
+
+	}
+
+	private List<File> getImagesFromFolder(File folder, String[] imageTypes) {
+		List<File> images = new ArrayList<File>();
+		File[] files = folder.listFiles();
+		for (File file : files) {
+			for (String format : imageTypes) {
+				if (hasFormat(file, format)) {
+					images.add(file);
+				}
+			}
+		}
+		return images;
+	}
+
+	private boolean hasFormat(File file, String format) {
+		return file.toString().toLowerCase().endsWith("." + format);
+	}
+	
+	public File[] getAllImagesFromFolder(File folder, String[] imageTypes) {
+		List<File> images = getImagesFromFolder(folder, imageTypes);
+		return images.toArray(new File[]{});
 	}
 
 }
--- controller/src/main/java/de/unigoettingen/sub/ocr/controller/OcrEngineStarter.java
+++ controller/src/main/java/de/unigoettingen/sub/ocr/controller/OcrEngineStarter.java
@@ -1,19 +1,74 @@
 package de.unigoettingen.sub.ocr.controller;
 
+import java.io.File;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
 import de.uni_goettingen.sub.commons.ocr.api.OCREngine;
+import de.uni_goettingen.sub.commons.ocr.api.OCRFormat;
+import de.uni_goettingen.sub.commons.ocr.api.OCRImage;
+import de.uni_goettingen.sub.commons.ocr.api.OCROutput;
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess;
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess.OCRPriority;
+import de.uni_goettingen.sub.commons.ocr.api.OCRProcess.OCRTextType;
 import de.uni_goettingen.sub.commons.ocr.api.OcrFactory;
 
 public class OcrEngineStarter {
 
-	private FactoryProvider provider = new FactoryProvider();
+	private FactoryProvider factoryProvider = new FactoryProvider();
+	private BeanProvider beanProvider = new BeanProvider();
 	
 	// for unit tests
 	void setFactoryProvider(FactoryProvider newProvider) {
-		provider = newProvider;
+		factoryProvider = newProvider;
+	}
+	// for unit tests
+	void setBeanProvider(BeanProvider newBeanProvider) {
+		beanProvider = newBeanProvider;
 	}
 	
 	public void startOcrWithParams(OcrParameters params) {
-		OcrFactory factory = provider.createFactory(params.ocrEngine);
+		OcrFactory factory = factoryProvider.createFactory(params.ocrEngine);
 		OCREngine engine = factory.createEngine();
+		
+		FileManager manager = beanProvider.getFileManager();
+		File[] allBookFolders = manager.getAllFolders(params.inputFolder, params.inputFormats);
+		for (File bookFolder : allBookFolders) {
+			OCRProcess process = factory.createProcess();
+			process.setName(bookFolder.getName());
+			File[] allPages = manager.getAllImagesFromFolder(bookFolder, params.inputFormats);
+			List<OCRImage> images = new ArrayList<OCRImage>();
+			for (File page : allPages) {
+				OCRImage image = factory.createImage();
+				image.setUri(page.toURI());
+				image.setSize(page.length());
+				images.add(image);
+			}
+			process.setOcrImages(images);
+			
+			for (String outFormat : params.outputFormats) {
+				OCRFormat ocrFormat = OCRFormat.valueOf(outFormat);
+				OCROutput output = factory.createOutput();
+				File outputFolder = new File(params.outputFolder);
+				URI outputUri = new File(outputFolder, process.getName() + "." + outFormat.toLowerCase()).toURI();
+				output.setUri(outputUri);
+				output.setlocalOutput(outputFolder.getAbsolutePath());
+				process.addOutput(ocrFormat, output);
+			}
+			
+			Set<Locale> languages = new HashSet<Locale>();
+			for (String lang : params.inputLanguages) {
+				languages.add(new Locale(lang));
+			}
+			process.setLanguages(languages);
+			process.setPriority(OCRPriority.NORMAL);
+			process.setTextType(OCRTextType.valueOf(params.inputTextType));
+			engine.addOcrProcess(process);
+		}
+		engine.recognize();
 	}
 }
