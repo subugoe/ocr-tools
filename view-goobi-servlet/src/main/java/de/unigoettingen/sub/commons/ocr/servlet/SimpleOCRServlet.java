9ef6c4f672f3582be4494afdd7d48e5688d42273
Dennis Neumann <neumann@sub.uni-goettingen.de>
2014-07-18 17:07:59 +0200

    goobi-servlet uses controller

--- view-goobi-servlet/src/main/java/de/unigoettingen/sub/commons/ocr/servlet/SimpleOCRServlet.java
+++ view-goobi-servlet/src/main/java/de/unigoettingen/sub/commons/ocr/servlet/SimpleOCRServlet.java
@@ -1,39 +1,23 @@
 package de.unigoettingen.sub.commons.ocr.servlet;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.FileUtils;
 
-import de.uni_goettingen.sub.commons.ocr.api.OCREngine;
-import de.uni_goettingen.sub.commons.ocr.api.OCRFormat;
-import de.uni_goettingen.sub.commons.ocr.api.OCRImage;
-import de.uni_goettingen.sub.commons.ocr.api.OCROutput;
-import de.uni_goettingen.sub.commons.ocr.api.OCRProcess;
-import de.uni_goettingen.sub.commons.ocr.api.OcrFactory;
-import de.uni_goettingen.sub.commons.ocr.api.OCRProcess.OCRPriority;
-import de.uni_goettingen.sub.commons.ocr.api.OCRProcess.OCRTextType;
-import de.unigoettingen.sub.commons.util.file.FileUtils;
-import de.unigoettingen.sub.ocr.controller.FactoryProvider;
+import de.unigoettingen.sub.ocr.controller.OcrEngineStarter;
 import de.unigoettingen.sub.ocr.controller.OcrParameters;
-
-import java.io.BufferedReader;
 
 public class SimpleOCRServlet extends HttpServlet {
 	
@@ -41,18 +25,26 @@ public class SimpleOCRServlet extends HttpServlet {
 	
 	private static final String TITLE = "GDZ Simple-OCR 0.0.3 - Java";	
 	
-	private static final Logger logger = LoggerFactory.getLogger(SimpleOCRServlet.class);
-
 	private String defaultLanguage;
-	private String cacheDir;
-	private String dirPrefix;
+	private String tempRootDir;
+	private String sourceRootDir;
 	private String fileExtension;
+	
+	// TODO: inline these
+	private String sourceImagesDir;
+	private String tempImagesDir;
+	private String tempResultsDir;
+
+	// for unit tests
+	protected OcrEngineStarter getEngineStarter() {
+		return new OcrEngineStarter();
+	}
 
 	@Override
 	public void init() throws ServletException {
-		defaultLanguage = getInitParam("defaultLang");
-		cacheDir = getInitParam("cacheDir");
-		dirPrefix = getInitParam("dirPrefix");
+		defaultLanguage = getInitParam("defaultLanguage");
+		tempRootDir = getInitParam("tempRootDir");
+		sourceRootDir = getInitParam("sourceRootDir");
 		fileExtension = getInitParam("fileExtension");		
 	}
 
@@ -63,7 +55,7 @@ public class SimpleOCRServlet extends HttpServlet {
 		}
 		return initParam;
 	}
-	
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		response.setContentType("text/html");
@@ -75,31 +67,40 @@ public class SimpleOCRServlet extends HttpServlet {
 		if (lang == null) {
 			lang = defaultLanguage;
         }
-		String path = request.getParameter("path");
-		if (path == null) {
+		String restOfSourceDir = request.getParameter("path");
+		if (restOfSourceDir == null) {
 			throw new ServletException("Fehlende Eingabedaten, z. B. ?path=myimages/ocr");
 		}
-		path = normalizePath(path);
+		restOfSourceDir = normalizePath(restOfSourceDir);
+		sourceImagesDir = sourceRootDir + restOfSourceDir;
+		tempImagesDir = tempRootDir + restOfSourceDir;
+		tempResultsDir = tempRootDir + restOfSourceDir;
+		
 		String imagesRange = request.getParameter("imgrange");
 		if (imagesRange == null) {
 			throw new ServletException("Fehlende Eingabedaten, z. B. ?imgrange=1-10");
 		}
-		List<String> files = createFileList(imagesRange);
-		copyFiles(files, path);
+		List<String> imageNames = createFileList(imagesRange);
+		copyFiles(imageNames);
 
-		String workDir = cacheDir + path;
 		List<File> images = new ArrayList<File>();
-		for (String file : files) {
-			images.add(new File(workDir + file));
+		for (String file : imageNames) {
+			images.add(new File(tempImagesDir + file));
 		}
 		
-		try {
-			ocr(images, lang, path);
-		} catch (URISyntaxException e) {
-			logger.warn("Malformed URL;", e);
-		}
+		OcrParameters params = new OcrParameters();
+		params.inputFolder = tempImagesDir;
+		params.outputFolder = tempResultsDir;
+		params.inputLanguages = new String[]{lang};
+		params.inputTextType = "NORMAL";
+		params.ocrEngine = "abbyy-multiuser";
+		params.outputFormats = new String[]{"TXT"};
+		params.priority = "1";
 		
-		pw.println(createResponse(path, getJobName(path)));
+		OcrEngineStarter engineStarter = getEngineStarter();
+		engineStarter.startOcrWithParams(params);
+		
+		pw.println(createResponse(getJobName()));
 
 	}
 
@@ -138,87 +139,28 @@ public class SimpleOCRServlet extends HttpServlet {
 		return pattern.replaceAll(regex, n + ".");
 	}
 
-	private boolean copyFiles(List<String> files, String path) throws IOException, ServletException {
-		String toDir = cacheDir + path + File.separator;
-		String fromDir = dirPrefix + path + File.separator;
+	private boolean copyFiles(List<String> files) throws IOException, ServletException {
 
-		if(!new File(fromDir).exists()) {
-			throw new ServletException("Source path does not exist: " + fromDir);
+		if(!new File(sourceImagesDir).exists()) {
+			throw new ServletException("Source path does not exist: " + sourceImagesDir);
 		}
 		
-		File tmp = new File(toDir);
-		if (!tmp.exists() && !tmp.mkdirs()) {
-			throw new ServletException("Couldn't create temp directory!");
-		}
 		for (String file : files) {
-			FileUtils.copyDirectory(new File(fromDir + file), new File(toDir + file));
+			FileUtils.copyFile(new File(sourceImagesDir + file), new File(tempImagesDir + file));
 		}
 		return true;
 	}
-
-	private void ocr(List<File> images, final String lang, String path) throws URISyntaxException {
-		FactoryProvider factoryProvider = new FactoryProvider();
-		OcrFactory factory = factoryProvider.createFactory("abbyy-multiuser", new Properties());
-		
-		String workDir = cacheDir + path;
-		File folder = new File(cacheDir + path);
-
-		OCREngine engine = factory.createEngine();
-
-		logger.debug("Creating Process for " + folder.toString());
-		OCRProcess aop = factory.createProcess();
-		String jobName = getJobName(path);
-		aop.setName(jobName);
-		if (jobName.equals("")) {
-			logger.error("Name for process not set, to avoid errors if your using parallel processes, we generate one.");
-			aop.setName(UUID.randomUUID().toString());
-		}
-		List<OCRImage> imgs = new ArrayList<OCRImage>();
-		for (File imageFile : images) {
-			OCRImage aoi = factory.createImage();
-			aoi.setUri(imageFile.toURI());
-			aoi.setSize(imageFile.length());
-			imgs.add(aoi);
-		}
-		aop.setOcrImages(imgs);
-
-		OCROutput aoo = factory.createOutput();						
-		URI uri = new URI(new File(workDir).toURI()
-				+ jobName
-				+ fileExtension);
-		logger.debug("output Location " + uri.toString());
-		aoo.setUri(uri);
-		aoo.setlocalOutput(new File(workDir).getAbsolutePath());
-		aop.addOutput(OCRFormat.TXT, aoo);
-		
-		aop.addLanguage(new Locale(lang));
-		aop.setTextType(OCRTextType.valueOf("NORMAL"));
-		aop.setPriority(OCRPriority.ABOVENORMAL);
-		engine.addOcrProcess(aop);
-
-		
-		
-		logger.info("Starting recognize method");
-		engine.recognize();
-		logger.debug("recognize Finished");
-
-	}
 	
-	
-	
-    private String getJobName(String path) {
-        File folder = new File(cacheDir + path);
+    private String getJobName() {
+        File folder = new File(sourceImagesDir);
         return folder.getName();
     }
-        
 
-	private String createResponse(String path, String jobName) throws IOException {
-		String workDir = cacheDir + path;
-
+	private String createResponse(String jobName) throws IOException {
 		StringBuilder response = new StringBuilder();
 		String line;
 		response.append("<pre>");
-		String txtfile = workDir + jobName + fileExtension;
+		String txtfile = tempResultsDir + jobName + fileExtension;
 		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(txtfile), "UTF-8"));
 		while ((line = in.readLine()) != null) {
 			response.append(line.replaceAll("(\\s+)", " ")).append("<br/>\n");
