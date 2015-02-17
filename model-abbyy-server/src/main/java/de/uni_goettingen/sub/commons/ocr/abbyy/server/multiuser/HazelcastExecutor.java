e4194a64c8b366714e182e2ee3ffe656d4a9f7e0
dennis <dennis@unknownc8bcc8e48ae9>
2015-02-17 19:50:56 +0100

    hazelcast executor should be testable now

--- model-abbyy-server/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/multiuser/HazelcastExecutor.java
+++ model-abbyy-server/src/main/java/de/uni_goettingen/sub/commons/ocr/abbyy/server/multiuser/HazelcastExecutor.java
@@ -7,12 +7,13 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.ICondition;
 import com.hazelcast.core.ILock;
 
 import de.uni_goettingen.sub.commons.ocr.abbyy.server.AbbyyProcess;
@@ -29,24 +30,30 @@ public class HazelcastExecutor extends ThreadPoolExecutor implements Executor {
 	private Map<String, AbbyyProcess> queuedProcesses;
 	private Set<String> runningProcesses;
 
-	private ILock clusterLock;
-	private ICondition mightBeAllowedToExecute;
-	private long waitingTimeInMillis = 1000 * 60 * 30;
+	private Lock clusterLock;
+	private Condition mightBeAllowedToExecute;
+	private long waitingTimeInMillis = 1000 * 60 * 10;
 
 	// for unit tests
 	void setWaitingTime(long newTime) {
 		waitingTimeInMillis = newTime;
 	}
+	Condition createCondition() {
+		if (mightBeAllowedToExecute != null) {
+			return mightBeAllowedToExecute;
+		} else {
+			return ((ILock)clusterLock).newCondition("clusterCondition");
+		}
+	}
 	
 	public HazelcastExecutor(int maxParallelThreads, HazelcastInstance hazelcast) {
-		super(maxParallelThreads, maxParallelThreads, 0L, TimeUnit.MILLISECONDS,
-				new LinkedBlockingQueue<Runnable>());
+		super(maxParallelThreads, maxParallelThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
 		clusterLock = hazelcast.getLock("clusterLock");
-		mightBeAllowedToExecute = clusterLock.newCondition("clusterCondition");
 		maxProcesses = maxParallelThreads;
 		queuedProcessesSorted = new PriorityQueue<AbbyyProcess>(100, new ItemComparator());
 
 		queuedProcesses = hazelcast.getMap("queued");
+		// do we really need this? As soon as beforeExecute() runs, there should be a slot
 		runningProcesses = hazelcast.getSet("running");
 	}
 
@@ -54,6 +61,7 @@ public class HazelcastExecutor extends ThreadPoolExecutor implements Executor {
 	protected void beforeExecute(Thread t, Runnable process) {
 		super.beforeExecute(t, process);
 		AbbyyProcess abbyyProcess = (AbbyyProcess) process;
+		mightBeAllowedToExecute = createCondition();
 
 		clusterLock.lock();
 		try {
